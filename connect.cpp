bb13f5e86a689b7c3992f31576fbd60a97c1b78c
MITSUNARI Shigeo <herumi@nifty.com>
2013-12-25 10:53:33 +0900

    ping sample

---
+++ connect.cpp
@@ -0,0 +1,47 @@
+#include <cybozu/option.hpp>
+#include <cybozu/socket.hpp>
+
+int main(int argc, char *argv[])
+	try
+{
+	bool isServer;
+	std::string ip;
+	uint16_t port;
+	std::string cmd;
+
+	cybozu::Option opt;
+	opt.appendOpt(&isServer, false, "s", "server");
+	opt.appendOpt(&ip, "", "ip", "ip address");
+	opt.appendMust(&port, "p", "port");
+	opt.appendParamOpt(&cmd, "cmd", "string to send");
+
+	if (!opt.parse(argc, argv)) {
+		opt.usage();
+		return 1;
+	}
+	if (isServer) {
+		printf("server port=%d\n", port);
+		cybozu::Socket server;
+		server.bind(port);
+		for (;;) {
+			while (!server.queryAccept()) {
+			}
+			cybozu::Socket client;
+			cybozu::SocketAddr addr;
+			server.accept(client, &addr);
+			printf("addr=%s\n", addr.toStr().c_str());
+			{
+				char buf[128];
+				size_t readSize = client.readSome(buf, sizeof(buf));
+				printf("rec=%s\n", std::string(buf, readSize).c_str());
+			}
+		}
+	} else {
+		printf("client ip=%s port=%d\n", ip.c_str(), port);
+		cybozu::Socket client;
+		client.connect(ip, port);
+		client.write(cmd.c_str(), cmd.size());
+	}
+} catch (std::exception& e) {
+	printf("ERR %s\n", e.what());
+}
