fc938af81ee64fde706cdee9f2f8149a95226d10
MITSUNARI Shigeo <herumi@nifty.com>
2014-06-12 12:19:37 +0900

    put version

--- crash-gdb.cpp
+++ crash-gdb.cpp
@@ -1,13 +1,15 @@
 /*
-	crash gdb sample code
+	a sample code to crash gdb 7.7
 	g++ crash-gdb.cpp -std=c++11 && gdb ./a.out
 	clang++ crash-gdb.cpp -std=c++11 && gdb ./a.out
 
-	g++ 4.8.2
-	clang 3.5-1ubuntu1
+	g++ 4.6.3 -std=c++0x
+	g++ 4.8.2 -std=c++11
+	clang 3.5-1ubuntu1 -std=c++11
 
-	gdb (Ubuntu 7.7-Oubuntu3.1) 7.7 ; crash
 	gdb (Ubuntu/Linaro 7.4-2012.04-0ubuntu2.1) 7.4-2012.04 ; not crash
+	gdb (Ubuntu 7.7-Oubuntu3.1) 7.7 ; crash
+	gdb 7.8.50.20140612-cvs ; not crash
 */
 #include <utility>
 #include <stdio.h>
@@ -18,7 +20,9 @@ template<class Func>
 struct Holder : public BaseHolder {
 	Func func;
 	explicit Holder(Func&& func)
-		: func(std::forward<Func>(func)) { }
+		: func(std::forward<Func>(func))
+	{
+	}
 };
 
 struct Runner {
@@ -40,5 +44,9 @@ int main()
 {
 	int a = 0;
 	f(a);
-	puts("--- ok ---");
+#ifdef __clang__
+	printf("clang %d %d\n", __clang_major__, __clang_minor__);
+#elif defined(__GNUC__)
+	printf("gcc %d %d\n", __GNUC__, __GNUC_MINOR__);
+#endif
 }
