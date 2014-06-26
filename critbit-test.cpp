33e943427c3a1c10bbce90568708ddd6b4fd1d3a
MITSUNARI Shigeo <herumi@nifty.com>
2014-06-26 17:13:58 +0900

    add reference of url

---
+++ critbit-test.cpp
@@ -0,0 +1,130 @@
+#include "critbit.hpp"
+#include <string>
+#include <stdlib.h>
+#include <set>
+#include <vector>
+#define CYBOZU_TEST_DISABLE_AUTO_RUN
+#include <cybozu/test.hpp>
+#include <cybozu/benchmark.hpp>
+#include <cybozu/mmap.hpp>
+
+struct StrSet : std::set<std::string> {
+	bool has(const std::string& str) const
+	{
+		return this->find(str) != this->end();
+	}
+};
+
+CYBOZU_TEST_AUTO(has)
+{
+	critbit::StrSet crit;
+	static const char* elems[] = {"a", "aa", "b", "bb", "ab", "ba", "aba", "bab", NULL};
+
+	for (size_t i = 0; elems[i]; ++i) crit.insert(elems[i]);
+		CYBOZU_TEST_EQUAL(crit.size(), 8);
+
+	for (size_t i = 0; elems[i]; ++i) {
+		CYBOZU_TEST_ASSERT(crit.has(elems[i]));
+	}
+}
+
+CYBOZU_TEST_AUTO(remove)
+{
+	static const char* elems[] = {"a", "aa", "b", "bb", "ab", "ba", "aba", "bab", NULL};
+
+	for (size_t i = 1; elems[i]; ++i) {
+		critbit::StrSet crit;
+		for (size_t j = 0; j < i; ++j) crit.insert(elems[j]);
+		CYBOZU_TEST_EQUAL(crit.size(), i);
+
+		for (size_t j = 0; j < i; ++j) {
+			CYBOZU_TEST_ASSERT(crit.has(elems[j]));
+		}
+
+		for (size_t j = 0; j < i; ++j) {
+			CYBOZU_TEST_ASSERT(crit.remove(elems[j]));
+		}
+		CYBOZU_TEST_EQUAL(crit.size(), 0);
+
+		for (size_t j = 0; j < i; ++j) {
+			CYBOZU_TEST_ASSERT(!crit.has(elems[j]));
+		}
+	}
+}
+
+static bool handler(void *s, const char* elem)
+{
+	((StrSet*)s)->insert(elem);
+	return true;
+}
+
+CYBOZU_TEST_AUTO(traverse)
+{
+	critbit::StrSet crit;
+	static const char* elems[] = {"a", "aa", "aaz", "abz", "bba", "bbc", "bbd", NULL};
+
+	for (size_t i = 0; elems[i]; ++i) crit.insert(elems[i]);
+	CYBOZU_TEST_EQUAL(crit.size(), 7);
+
+	StrSet s;
+	crit.traverse("a", &s, handler);
+
+	CYBOZU_TEST_EQUAL(s.size(), 4);
+	CYBOZU_TEST_ASSERT(s.find("a") != s.end());
+	CYBOZU_TEST_ASSERT(s.find("aa") != s.end());
+	CYBOZU_TEST_ASSERT(s.find("aaz") != s.end());
+	CYBOZU_TEST_ASSERT(s.find("abz") != s.end());
+
+	s.clear();
+	crit.traverse("aa", &s, handler);
+
+	CYBOZU_TEST_EQUAL(s.size(), 2);
+	CYBOZU_TEST_ASSERT(s.find("aa") != s.end());
+	CYBOZU_TEST_ASSERT(s.find("aaz") != s.end());
+}
+
+template<class Set>
+void bench(const char *msg, const std::string& text)
+{
+	printf("bench:%s\n", msg);
+	std::vector<std::string> sampling;
+	Set set;
+	std::istringstream iss(text);
+	std::string word;
+	int c = 0;
+	while (iss >> word) {
+		set.insert(word);
+		c++;
+		if ((c % 100) == 0 && sampling.size() < 10) {
+			sampling.push_back(word);
+		}
+	}
+	printf("word num %d\n", (int)set.size());
+	printf("sampling num %d\n", (int)sampling.size());
+	for (size_t i = 0; i < sampling.size(); i++) {
+		const std::string w = sampling[i];
+		const char *msg = sampling[i].c_str();
+		CYBOZU_BENCH(msg, set.has, w);
+	}
+	const std::string str = "maybe-not-found-string";
+	CYBOZU_BENCH(str.c_str(), set.has, str);
+}
+
+int main(int argc, char *argv[])
+	try
+{
+	if (argc == 2) {
+		const std::string file = argv[1];
+		printf("load file %s\n", file.c_str());
+		cybozu::Mmap m(file);
+		const std::string text(m.get(), m.size());
+		bench<StrSet>("std::set", text);
+		bench<critbit::StrSet>("critbit::set", text);
+		bench<StrSet>("std::set", text);
+		bench<critbit::StrSet>("critbit::set", text);
+	}
+	return cybozu::test::autoRun.run(argc, argv);
+} catch (std::exception& e) {
+	printf("ERR %s\n", e.what());
+	return 1;
+}
---
+++ critbit.hpp
@@ -0,0 +1,225 @@
+/*
+	original code is https://github.com/agl/critbit
+*/
+#pragma once
+#include <stdint.h>
+#include <string.h>
+#include <new>
+#include <string>
+#define CRITBIT_USE_BSF
+
+#ifdef CRITBIT_USE_BSF
+#include <cybozu/bit_operation.hpp>
+#endif
+
+namespace critbit {
+
+struct StrSet {
+	StrSet()
+		: root_(0)
+		, size_(0)
+	{
+	}
+	~StrSet()
+	{
+		if (!root_) return;
+		clearAll(root_);
+	}
+	size_t size() const { return size_; }
+	bool insert(const std::string& str)
+	{
+		return append(str.c_str(), str.size());
+	}
+	bool append(const char *s, size_t len = 0)
+	{
+		const uint8_t* u = reinterpret_cast<const uint8_t*>(s);
+		if (len == 0) len = strlen(s);
+		if (!root_) {
+			root_ = (uint8_t*)malloc(len + 1);
+			if (root_ == 0) throw std::bad_alloc();
+			memcpy(root_, u, len + 1);
+			size_++;
+			return true;
+		}
+		uint8_t *p = seekToLeaf(root_, u, len);
+	
+		uint32_t newLen;
+		uint32_t extra;
+	
+		for (newLen = 0; newLen < len; newLen++) {
+			if (p[newLen] != u[newLen]) {
+				extra = p[newLen] ^ u[newLen];
+				goto different_byte_found;
+			}
+		}
+		if (p[newLen]) {
+			extra = p[newLen];
+			goto different_byte_found;
+		}
+		return false; // already has s
+	different_byte_found:
+		extra = calcBit(extra);
+		const uint8_t c = p[newLen];
+		const int newdirection = (1 + (extra | c)) >> 8;
+
+		Node* const newNode = (Node*)malloc(sizeof(Node));
+		if (newNode == 0) throw std::bad_alloc();
+
+		uint8_t *const x = (uint8_t*)malloc(len + 1);
+		if (x == 0) {
+			free(newNode);
+			throw std::bad_alloc();
+		}
+		memcpy(x, u, len + 1);
+		newNode->len_ = newLen;
+		newNode->extra_ = extra;
+		newNode->child_[1 - newdirection] = x;
+		uint8_t** wherep = &root_;
+	
+		for (;;) {
+			uint8_t* p = *wherep;
+	
+			if (!isNode(p)) break;
+			Node* q = (Node*)(p - 1);
+	
+			if (q->len_ > newLen) break;
+			if (q->len_ == newLen && q->extra_ > extra) break;
+
+			wherep = &q->child_[q->getDirection(u, len)];
+		}
+	
+		newNode->child_[newdirection] = *wherep;
+		*wherep = (uint8_t*)(1 + (char*)newNode);
+		size_++;
+		return true;
+	}
+	bool has(const std::string& str) const
+	{
+		return has(str.c_str(), str.size());
+	}
+	bool has(const char *s, size_t len = 0) const
+	{
+		if (!root_) return false;
+		const uint8_t* u = reinterpret_cast<const uint8_t*>(s);
+		if (len == 0) len = strlen(s);
+		uint8_t *p = seekToLeaf(root_, u, len);
+		return strcmp(s, (const char*)p) == 0;
+	}
+	bool remove(const std::string& str)
+	{
+		return remove(str.c_str(), str.size());
+	}
+	bool remove(const char *s, size_t len = 0)
+	{
+		if (!root_) return false;
+		const uint8_t* u = reinterpret_cast<const uint8_t*>(s);
+		if (len == 0) len = strlen(s);
+		uint8_t* p = root_;
+		uint8_t** wherep = &root_;
+		uint8_t** whereq = 0;
+		Node* q = 0;
+		int direction = 0;
+	
+		while (isNode(p)) {
+			q = (Node*)(p - 1);
+			direction = q->getDirection(u, len);
+			whereq = wherep;
+			wherep = &q->child_[direction];
+			p = *wherep;
+		}
+		if (strcmp(s, (const char*)p) != 0) return false;
+		size_--;
+		free(p);
+		if (!whereq) {
+			root_ = 0;
+			return true;
+		}
+		*whereq = q->child_[1 - direction];
+		free(q);
+		return true;
+	}
+	/*
+		search string having prefix
+		stop traversing if handler returns false
+	*/
+	bool traverse(const char* prefix, void *arg, bool handler(void *, const char*))
+	{
+		if (!root_) return 1;
+		const uint8_t* u = reinterpret_cast<const uint8_t*>(prefix);
+		const size_t len = strlen(prefix);
+		uint8_t* p = root_;
+		uint8_t* top = p;
+	
+		while (isNode(p)) {
+			Node* q = (Node*)(p - 1);
+			p = q->child_[q->getDirection(u, len)];
+			if (q->len_ < len) top = p;
+		}
+		for (size_t i = 0; i < len; i++) {
+			if (p[i] != u[i]) return 1;
+		}
+		return traverseInner(top, arg, handler);
+	}
+private:
+	uint8_t *root_;
+	size_t size_;
+	struct Node {
+		uint8_t* child_[2];
+		uint32_t len_;
+		uint8_t extra_;
+		int getDirection(const uint8_t *u, size_t len) const
+		{
+			const uint8_t c = (len_ < len) ? u[len_] : 0;
+			return (1 + (extra_ | c)) >> 8;
+		}
+	};
+	static inline bool isNode(const uint8_t *p)
+	{
+		return ((intptr_t)p & 1) != 0;
+	}
+	uint8_t *seekToLeaf(uint8_t *p, const uint8_t *u, size_t len) const
+	{
+		while (isNode(p)) {
+			Node* q = (Node*)(p - 1);
+			p = q->child_[q->getDirection(u, len)];
+		}
+		return p;
+	}
+	
+	static void clearAll(uint8_t* p)
+	{
+		if (isNode(p)) {
+			Node* q = (Node*)(p - 1);
+			clearAll(q->child_[0]);
+			clearAll(q->child_[1]);
+			free(q);
+		} else {
+			free(p);
+		}
+	}
+	template<class T>
+	inline bool traverseInner(uint8_t* p, T *arg, bool handler(T *, const char*))
+	{
+		if (isNode(p)) {
+			Node* q = (Node*)(p - 1);
+
+			if (!traverseInner(q->child_[0], arg, handler)) return false;
+			return traverseInner(q->child_[1], arg, handler);
+		}
+		return handler(arg, (const char*)p);
+	}
+	uint32_t calcBit(uint8_t x) const
+	{
+#ifdef CRITBIT_USE_BSF
+		if (x == 0) return 255;
+		return 255 - (1 << cybozu::bsr(x));
+#else
+		x |= x >> 1;
+		x |= x >> 2;
+		x |= x >> 4;
+		return (x & ~(x >> 1)) ^ 255;
+#endif
+	}
+};
+
+} // critbit
