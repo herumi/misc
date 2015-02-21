4f1bcc936e6893b4baebf45f36a481765c955a30
MITSUNARI Shigeo <herumi@nifty.com>
2015-02-22 05:58:57 +0900

    bench mutex without conflict

--- atomic_vs_mutex.cpp
+++ atomic_vs_mutex.cpp
@@ -23,21 +23,31 @@ std::atomic<int> ai;
 void atomic_inc() { ai++; }
 int atomic_get() { return (int)ai; }
 
-std::mutex m;
+std::atomic<int> bi;
+void atomic_inc2() { bi++; }
+
 int mi;
+std::mutex m;
 void mutex_inc()
 {
 	std::lock_guard<std::mutex> al(m);
 	mi++;
 }
 int mutex_get() { return mi; }
+int mi2;
+std::mutex m2;
+void mutex_inc2()
+{
+	std::lock_guard<std::mutex> al(m2);
+	mi2++;
+}
 
-void bench(const char *msg, void f(), int get())
+void bench(const char *msg, void f(), int get(), void f2() = 0)
 {
 	Xbyak::util::Clock clk;
 	clk.begin();
 	std::thread t1(loop, f);
-	std::thread t2(loop, f);
+	std::thread t2(loop, f2 ? f2 : f);
 	t1.join();
 	t2.join();
 	clk.end();
@@ -51,4 +61,6 @@ int main()
 	bench("none  ", none_inc, none_get);
 	bench("atomic", atomic_inc, atomic_get);
 	bench("mutex ", mutex_inc, mutex_get);
+	bench("atomic not conflict", atomic_inc, atomic_get, atomic_inc2);
+	bench("mutex  not conflict", mutex_inc, mutex_get, mutex_inc2);
 }
