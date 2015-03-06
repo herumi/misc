2980df4b46252adfd3da55d0d64228abb7363da1
MITSUNARI Shigeo <herumi@nifty.com>
2015-03-06 10:25:45 +0900

    set parameter for hash

--- consistent_hash.cpp
+++ consistent_hash.cpp
@@ -18,7 +18,6 @@ size_t ConsistentHash(uint64_t key, size_t n) {
 typedef std::set<size_t> IntSet;
 typedef std::map<size_t, IntSet> Map;
 
-const int N = 10000;
 
 void put(const Map& m)
 {
@@ -31,7 +30,7 @@ void put(const Map& m)
 	}
 }
 
-Map dump(size_t n)
+Map make(size_t n, size_t N)
 {
 	Map m;
 	for (size_t key = 0; key < N; key++) {
@@ -58,13 +57,14 @@ int diff(const Map& m1, const Map& m2)
 	return moved;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-	const int n1 = 100;
-	const int n2 = 120;
-	auto m1 = dump(n1);
+	const int N = argc >= 2 ? atoi(argv[1]) : 10000;
+	const int n1 = argc >= 3 ? atoi(argv[2]) : 100;
+	const int n2 = argc >= 4 ? atoi(argv[3]) : 120;
+	auto m1 = make(n1, N);
 	puts("--");
-	auto m2 = dump(n2);
+	auto m2 = make(n2, N);
 	int moved = diff(m1, m2);
 	printf("expect %d %d\n", int((1 - n1 / double(n2)) * N), moved);
 }
