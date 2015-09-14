5484c6e6e48aa3ffaeb29ebb21b5b71e85300429
MITSUNARI Shigeo <herumi@nifty.com>
2015-09-14 15:24:36 +0900

    sample of item42

---
+++ cpp/item42.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <vector>
+
+struct A {
+	A() {}
+	A(const A&) { puts("cstr:const A&"); }
+	A(A&&) { puts("cstr:A&&"); }
+	A(const char*) { puts("cstr:const char*"); }
+	A& operator=(const A&) { puts("=:const A&"); return *this; }
+	A& operator=(A&&) { puts("=:A&&"); return *this; }
+	A& operator=(const char*) { puts("=:const char*"); return *this; }
+	~A() { puts("dstr"); }
+};
+
+int main()
+{
+	using Vec = std::vector<A>;
+	puts("--------------");
+	puts("empty push_back");
+	{
+		Vec v;
+		v.push_back("abc");
+	}
+	puts("--------------");
+	puts("empty emplace_back");
+	{
+		Vec v;
+		v.emplace_back("abc");
+	}
+		puts("--------------");
+	{
+		Vec v(1);
+		puts("--------------");
+		puts("non empty push_back");
+		v.insert(v.begin(), "abc");
+		puts("--------------");
+	}
+		puts("--------------");
+	{
+		Vec v(1);
+		puts("--------------");
+		puts("non empty emplace_back");
+		v.emplace(v.begin(), "abc");
+		puts("--------------");
+	}
+}
