32e4dfefb1ae320670b977d7ba754a6e1ad8a88b
MITSUNARI Shigeo <herumi@nifty.com>
2015-11-09 10:51:03 +0900

    exec arguments

--- go/seccon2013/exec.cpp
+++ go/seccon2013/exec.cpp
@@ -1,42 +1,30 @@
 #include <stdio.h>
 #include <string>
 #include <cybozu/atoi.hpp>
-#include <cybozu/mmap.hpp>
 #define XBYAK_NO_OP_NAMES
 #include <xbyak/xbyak.h>
 
 struct A : Xbyak::CodeGenerator {
-	A(const char *p, size_t n)
+	explicit A(const std::string& s)
 	{
-		for (size_t i = 0; i < n; i += 2) {
-			uint8_t v = cybozu::hextoi(p + i, 2);
-			fprintf(stderr, "%02x ", v);
+		for (size_t i = 0; i < s.size(); i += 2) {
+			uint8_t v = cybozu::hextoi(s.c_str() + i, 2);
+//			fprintf(stderr, "%02x ", v);
 			db(v);
 		}
-		fprintf(stderr, "\n");
+//		fprintf(stderr, "\n");
 	}
 };
 
 int main(int argc, char *argv[])
 	try
 {
-	if (argc != 3) {
-		puts("exec line file number");
+	if (argc != 2) {
+		puts("exec hexbinary");
 		return 1;
 	}
-	const std::string file = argv[1];
-	const size_t n = atoi(argv[2]);
-
-	cybozu::Mmap m(file);
-	const char *p = m.get();
-	size_t size = m.size();
-	for (size_t i = 0; i <= n; i++) {
-		const char *q = (const char*)memchr(p, ' ', size);
-		if (q == NULL) return 1;
-		size -= (q + 1) - p;
-		p = q + 1;
-	}
-	A a(p, 62);
+	const std::string s = argv[1];
+	A a(s);
 	a.getCode<void(*)()>()();
 } catch (std::exception& e) {
 	fprintf(stderr, "err %s\n", e.what());
