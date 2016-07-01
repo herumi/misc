f67521e411745c01fd60a393ac4d5ee9f3fff2ab
MITSUNARI Shigeo <herumi@nifty.com>
2016-07-02 04:01:13 +0900

    run demo

--- emb/t.cpp
+++ emb/t.cpp
@@ -6,10 +6,10 @@
 int main(int argc, char *argv[])
 {
 	if (argc == 1) return 1;
-	std::fstream ifs(argv[1], std::ios::binary);
-	std::string s;
+	std::ifstream ifs(argv[1], std::ios::binary);
 	typedef std::map<std::string, int> Str2Int;
 	Str2Int m;
+	std::string s;
 	while (ifs >> s) {
 		m[s]++;
 	}
--- emb/test.sh
+++ emb/test.sh
@@ -1,14 +1,15 @@
 #objdump -CS -M intel --no-show-raw-insn ./a.out > a.asm
 g++ t.cpp -m32
-./a.out
+./a.out t.cpp > a.txt
 python embed-str.py a.out
 
 python embed-str.py a.out -o emb -s hello
 python embed-str.py emb
 
 chmod +x emb
-./emb
+./emb t.cpp > b.txt
 
 objdump -D --no-show-raw-insn ./a.out > a.asm
 objdump -D --no-show-raw-insn ./emb > b.asm
 diff a.asm b.asm
+diff a.txt b.txt
