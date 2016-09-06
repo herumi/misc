766ccd463e226404a1ad5b37bd5c68493dc97b0f
MITSUNARI Shigeo <herumi@nifty.com>
2016-09-06 21:40:28 +0900

    make []*C.A

--- go/vector.go
+++ go/vector.go
@@ -4,12 +4,15 @@ package main
 #include <stdio.h>
 #include <malloc.h>
 struct A {
-	char buf[8];
+	int x;
+	int y;
 };
 typedef struct A A;
-A *createA()
+A *createA(int x)
 {
 	A *p = calloc(sizeof(A), 1);
+	p->x = x;
+	p->y = x;
 	return p;
 }
 
@@ -20,12 +23,20 @@ void destroyA(A *a)
 
 void putA(const A *a)
 {
-	printf("addr %p\n", a);
+	printf("addr %p %d\n", a, a->x);
+}
+void putV(const A *const *a, size_t n)
+{
+	size_t i = 0;
+	for (i = 0; i < n; i++) {
+		putA(a[i]);
+	}
 }
 */
 import "C"
 import "fmt"
 import "runtime"
+import "unsafe"
 
 type A struct {
 	self *C.A
@@ -34,9 +45,9 @@ type A struct {
 func destroyA(a *A) {
 	C.destroyA(a.self)
 }
-func newA() *A {
+func newA(x int) *A {
 	a := new(A)
-	a.self = C.createA()
+	a.self = C.createA(C.int(x))
 	runtime.SetFinalizer(a, destroyA)
 	return a
 }
@@ -51,7 +62,7 @@ func main() {
 	{
 		v := make([]A, n)
 		for i := 0; i < len(v); i++ {
-			v[i] = *newA()
+			v[i] = *newA(i)
 		}
 		fmt.Println("&v   =", &v)
 		fmt.Println("&v[0]=", &v[0])
@@ -63,12 +74,22 @@ func main() {
 	{
 		v := make([]*A, 10)
 		for i := 0; i < len(v); i++ {
-			v[i] = newA()
+			v[i] = newA(i)
 		}
 		fmt.Println("&v   =", &v)
 		fmt.Println("&v[0]=", &v[0])
 		for i := 0; i < len(v); i++ {
 			v[i].put()
 		}
+	}
+	fmt.Println("make([]*C.A, n)")
+	{
+		v := make([]*C.A, n)
+		for i := 0; i < len(v); i++ {
+			v[i] = newA(i).self
+		}
+		fmt.Println("&v   =", &v)
+		fmt.Println("&v[0]=", &v[0])
+		C.putV((**C.A)(unsafe.Pointer(&v[0])), C.size_t(n))
 	}
 }
