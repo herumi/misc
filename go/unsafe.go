0c0dbc4cec1c31d5fca19ca56c019632b3da943e
MITSUNARI Shigeo <herumi@nifty.com>
2017-05-28 11:23:37 +0900

    (*C.void) is error

--- go/unsafe.go
+++ go/unsafe.go
@@ -2,28 +2,33 @@ package main
 
 /*
 #include <stdio.h>
+#include <stdint.h>
 typedef struct A {
-	int x[2];
+	uint64_t x[2];
 }A ;
 void putA(const A *a)
 {
 	printf("a=%p\n", a);
-	printf("x=%d %d\n", a->x[0], a->x[1]);
+	printf("x=%d %d\n", (int)a->x[0], (int)a->x[1]);
 }
 typedef struct B {
-	int y[2];
+	uint64_t y[2];
 }B ;
 void putB(const B *b)
 {
 	printf("b=%p\n", b);
-	printf("y=%d %d\n", b->y[0], b->y[1]);
+	printf("y=%d %d\n", (int)b->y[0], (int)b->y[1]);
+}
+void putC(const void *p)
+{
+	putB((const B*)p);
 }
 */
 import "C"
 import  "unsafe"
 
 type A struct {
-	x [2]int
+	x [2]uint64
 }
 
 func (a *A) Put() {
@@ -31,11 +36,15 @@ func (a *A) Put() {
 }
 
 type B struct {
-	y [2]int
+	y [2]uint64
 }
 
 func (b *B) Put() {
 	C.putB((*C.B)(unsafe.Pointer(&b.y[0])))
+}
+func (b *B) PutC() {
+//	C.putC((*C.uint8_t)(unsafe.Pointer(&b.y[0])))
+	C.putC((unsafe.Pointer(&b.y[0])))
 }
 
 
@@ -47,4 +56,8 @@ func main() {
 	v[0].Put()
 	w := *(*[]B)(unsafe.Pointer(&v))
 	w[0].Put()
+	ww := new(B)
+	ww.y[0] = 5
+	ww.y[1] = 8
+	ww.PutC()
 }
