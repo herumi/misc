56fa6ee4c826c9d16ca2ff0c76e0d931a8cb519c
MITSUNARI Shigeo <herumi@nifty.com>
2018-04-14 01:02:38 +0900

    answer of quiz

--- codec-calc.cpp
+++ codec-calc.cpp
@@ -4,7 +4,7 @@
 int calc(int a, int b, int s)
 {
 	const int Q = 1 << s, Q2 = Q * 2, Q3 = Q * 3;
-	assert(0 <= s && s <= 16 && b < a && a < Q * 4);
+	assert(0 <= s && s <= 16 && 0 <= b && b < a && a < Q * 4);
 	int n = 0;
 	for (;;) {
 		if (a < Q2) {
@@ -25,6 +25,22 @@ int calc(int a, int b, int s)
 	return n;
 }
 
+static inline int bsr(int x)
+{
+	assert(x > 0);
+	union {
+		int i;
+		float f;
+	} ui;
+	ui.f = (float)x;
+	return (ui.i >> 23) - 127; /* equal to "bsr eax, x" */
+}
+
+int calc2(int a, int b, int)
+{
+	return b >> (bsr(a ^ b) + 1);
+}
+
 int main()
 {
 	int s = 3;
@@ -32,6 +48,7 @@ int main()
 		printf("a=%2d ", a);
 		for (int b = 0; b < a; b++) {
 			printf("%2d ", calc(a, b, s));
+			if (calc(a, b, s) != calc2(a, b, s)) puts("ERR");
 		}
 		printf("\n");
 	}
