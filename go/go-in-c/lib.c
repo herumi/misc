77a2198d825376e558efc866bbc2631fdbfdb482
MITSUNARI Shigeo <herumi@nifty.com>
2019-01-22 21:03:13 +0900

    use byte instead of C.char

--- go/go-in-c/lib.c
+++ go/go-in-c/lib.c
@@ -2,6 +2,7 @@
 
 extern void GoFunc1();
 extern void GoFunc2(const void *buf, int n);
+extern void GoFunc3(void (*)(void*, int), void *buf, int n);
 
 void fill(void *buf, int n)
 {
@@ -33,4 +34,22 @@ void putput(const void *buf, int n)
 	}
 	printf("\n");
 	GoFunc2(buf, n);
+}
+
+#define S_BUF_SIZE 8
+static char s_buf[S_BUF_SIZE];
+
+void callGoFunc3(void (*f)(void *, int))
+{
+	printf("callGoFunc3\n");
+	GoFunc3(f, s_buf, S_BUF_SIZE);
+}
+
+void putBuf()
+{
+	printf("putBuf ");
+	for (int i = 0; i < S_BUF_SIZE; i++) {
+		printf("%02x ", (unsigned char)s_buf[i]);
+	}
+	printf("\n");
 }
--- go/go-in-c/main.go
+++ go/go-in-c/main.go
@@ -7,6 +7,8 @@ void ggg(void f(void *, int), void *buf, int n);
 void hhh();
 int fff();
 void putput(const void *buf, int n);
+void putBuf();
+void callGoFunc3(void (*f)(void *), void *buf, int n);
 #cgo LDFLAGS:-L./ -llib
 */
 import "C"
@@ -39,28 +41,41 @@ func GoFunc2(buf *C.char, n C.int) {
 	size := int(n)
 	fmt.Printf("buf=%p\n", buf)
 	fmt.Printf("n=%d\n", n)
-	slice := (*[1 << 30]C.char)(unsafe.Pointer(buf))[:size:size]
+	slice := (*[1 << 30]byte)(unsafe.Pointer(buf))[:size:size]
 	for i := 0; i < size; i++ {
 		fmt.Printf("%02x ", slice[i])
 	}
 	fmt.Printf("\n")
+	fmt.Printf("slice=%x\n", slice)
+}
+
+//export GoFunc3
+func GoFunc3(callback func([]byte), buf *C.char, n C.int) {
+	fmt.Printf("call GoFunc3\n")
+	size := int(n)
+	slice := (*[1 << 30]byte)(unsafe.Pointer(buf))[:size:size]
+	fmt.Printf("slice=%x\n", slice)
+	callback(slice)
 }
 
 func main() {
-	fmt.Printf("%d\n", C.fff());
+	fmt.Printf("%d\n", C.fff())
 	s := make([]byte, 4)
-	fmt.Printf("s=%x\n", s);
+	fmt.Printf("s=%x\n", s)
 	C.fill(unsafe.Pointer(&s[0]), C.int(len(s)))
 //	C.ggg(C.fill, unsafe.Pointer(&s[0]), C.int(len(s)))
-	fmt.Printf("s=%x\n", s);
+	fmt.Printf("s=%x\n", s)
 	Fill(s, len(s))
-	fmt.Printf("s=%x\n", s);
+	fmt.Printf("s=%x\n", s)
 	rand.Read(s)
-	fmt.Printf("s=%x\n", s);
+	fmt.Printf("s=%x\n", s)
 	var op Op
 	op.f = Fill
 	op.f(s, len(s))
-	fmt.Printf("s=%x\n", s);
+	fmt.Printf("s=%x\n", s)
 	C.hhh() // call GoFunc1
 	C.putput(unsafe.Pointer(&s[0]), C.int(len(s)))
+	C.putBuf();
+//	C.callGoFunc3(op.f)
+	C.putBuf();
 }
