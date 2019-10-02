4556b04947fe802e710a185bbcb9436b3570186c
MITSUNARI Shigeo <herumi@nifty.com>
2019-10-02 14:49:18 +0900

    add mcl::init

--- rust/mcl/src/lib.rs
+++ rust/mcl/src/lib.rs
@@ -1,3 +1,36 @@
+use std::os::raw::{c_int};
+
+#[link(name = "mcl", kind = "static")]
+#[link(name = "mclbn384_256", kind = "static")]
+#[link(name = "gmp")]
+#[link(name = "stdc++")]
+#[link(name = "crypto")]
+extern "C" {
+	fn mclBn_getVersion() -> c_int;
+	fn mclBn_init(curve : c_int, compiledTimeVar : c_int) -> c_int;
+}
+
+pub enum CurveType {
+	BN254 = 0,
+	BN381 = 1,
+	SNARK = 4,
+	BLS12_381 = 5,
+}
+
+const MCLBN_FP_UNIT_SIZE:usize = 6;
+const MCLBN_FR_UNIT_SIZE:usize = 4;
+const MCLBN_COMPILED_TIME_VAR:c_int = (MCLBN_FR_UNIT_SIZE as c_int * 10 + MCLBN_FP_UNIT_SIZE as c_int);
+
+pub struct Fp {
+	#[allow(dead_code)]
+	d:[u64; MCLBN_FP_UNIT_SIZE]
+}
+
+pub struct Fr {
+	#[allow(dead_code)]
+	d:[u64; MCLBN_FR_UNIT_SIZE]
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -8,4 +41,17 @@ mod tests {
 
 pub fn hello() -> String {
 	"abc".to_string()
+}
+
+pub fn get_version() -> u32 {
+	unsafe {
+		mclBn_getVersion() as u32
+	}
+}
+
+pub fn init(curve:CurveType) -> bool {
+	unsafe {
+		let r = mclBn_init(curve as c_int, MCLBN_COMPILED_TIME_VAR);
+		r as u32 == 0
+	}
 }
--- rust/mcl/src/main.rs
+++ rust/mcl/src/main.rs
@@ -1,19 +1,10 @@
 // env RUSTFLAGS="-L ../../../mcl/lib" cargo run
-use std::os::raw::{c_int};
-extern crate mcl;
 
-#[link(name = "mcl", kind = "static")]
-#[link(name = "mclbn384_256", kind = "static")]
-#[link(name = "gmp")]
-#[link(name = "stdc++")]
-#[link(name = "crypto")]
-
-extern "C" {
-	fn mclBn_getVersion() -> c_int;
-}
 fn main() {
 	println!("{}", mcl::hello());
-	unsafe {
-		println!("version={:04x}", mclBn_getVersion());
+	println!("version={:04x}", mcl::get_version());
+	let b = mcl::init(mcl::CurveType::BN254);
+	if !b {
+		println!("init err");
 	}
 }
