d821ee084b3691e049bdcdbd45281c1ad7a5cd80
MITSUNARI Shigeo <herumi@nifty.com>
2019-10-10 11:18:41 +0900

    add prototypes

--- rust/mcl/src/lib.rs
+++ rust/mcl/src/lib.rs
@@ -12,15 +12,8 @@ extern "C" {
     fn mclBn_getFrByteSize() -> u32;
     fn mclBn_getFpByteSize() -> u32;
     fn mclBn_init(curve: c_int, compiledTimeVar: c_int) -> c_int;
-    fn mclBnFr_setInt32(x: *mut Fr, v: i32);
-    fn mclBnFr_setStr(x: *mut Fr, buf: *const u8, bufSize: usize, ioMode: i32) -> c_int;
-    fn mclBnFr_getStr(buf: *mut u8, maxBufSize: usize, x: *const Fr, ioMode: i32) -> usize;
-    fn mclBnFr_serialize(buf: *mut u8, maxBufSize: usize, x: *const Fr) -> usize;
-    fn mclBnFr_deserialize(x: *mut Fr, buf: *const u8, bufSize: usize) -> usize;
-    fn mclBnFr_setLittleEndian(x: *mut Fr, buf: *const u8, bufSize: usize) -> i32;
-    fn mclBnFr_setLittleEndianMod(x: *mut Fr, buf: *const u8, bufSize: usize) -> i32;
-    fn mclBnFr_setHashOf(x: *mut Fr, buf: *const u8, bufSize: usize) -> i32;
-    fn mclBnFr_setByCSPRNG(x: *mut Fr);
+
+	// Fr
     fn mclBnFr_isEqual(x: *const Fr, y: *const Fr) -> i32;
     fn mclBnFr_isValid(x: *const Fr) -> i32;
     fn mclBnFr_isZero(x: *const Fr) -> i32;
@@ -28,15 +21,92 @@ extern "C" {
     fn mclBnFr_isOdd(x: *const Fr) -> i32;
     fn mclBnFr_isNegative(x: *const Fr) -> i32;
 
+    fn mclBnFr_setStr(x: *mut Fr, buf: *const u8, bufSize: usize, ioMode: i32) -> c_int;
+    fn mclBnFr_getStr(buf: *mut u8, maxBufSize: usize, x: *const Fr, ioMode: i32) -> usize;
+    fn mclBnFr_serialize(buf: *mut u8, maxBufSize: usize, x: *const Fr) -> usize;
+    fn mclBnFr_deserialize(x: *mut Fr, buf: *const u8, bufSize: usize) -> usize;
+
+    fn mclBnFr_setInt32(x: *mut Fr, v: i32);
+    fn mclBnFr_setLittleEndian(x: *mut Fr, buf: *const u8, bufSize: usize) -> i32;
+    fn mclBnFr_setLittleEndianMod(x: *mut Fr, buf: *const u8, bufSize: usize) -> i32;
+    fn mclBnFr_setHashOf(x: *mut Fr, buf: *const u8, bufSize: usize) -> i32;
+    fn mclBnFr_setByCSPRNG(x: *mut Fr);
+
     fn mclBnFr_add(z: *mut Fr, x: *const Fr, y: *const Fr);
     fn mclBnFr_sub(z: *mut Fr, x: *const Fr, y: *const Fr);
+    fn mclBnFr_neg(y: *mut Fr, x: *const Fr);
+
     fn mclBnFr_mul(z: *mut Fr, x: *const Fr, y: *const Fr);
     fn mclBnFr_div(z: *mut Fr, x: *const Fr, y: *const Fr);
-    fn mclBnFr_neg(y: *mut Fr, x: *const Fr);
+    fn mclBnFr_inv(y: *mut Fr, x: *const Fr);
     fn mclBnFr_sqr(y: *mut Fr, x: *const Fr);
     fn mclBnFr_squareRoot(y: *mut Fr, x: *const Fr) -> i32;
 
+	// Fp
+    fn mclBnFp_isEqual(x: *const Fp, y: *const Fp) -> i32;
+    fn mclBnFp_isValid(x: *const Fp) -> i32;
+    fn mclBnFp_isZero(x: *const Fp) -> i32;
+    fn mclBnFp_isOne(x: *const Fp) -> i32;
+    fn mclBnFp_isOdd(x: *const Fp) -> i32;
+    fn mclBnFp_isNegative(x: *const Fp) -> i32;
+
+    fn mclBnFp_setStr(x: *mut Fp, buf: *const u8, bufSize: usize, ioMode: i32) -> c_int;
+    fn mclBnFp_getStr(buf: *mut u8, maxBufSize: usize, x: *const Fp, ioMode: i32) -> usize;
+    fn mclBnFp_serialize(buf: *mut u8, maxBufSize: usize, x: *const Fp) -> usize;
+    fn mclBnFp_deserialize(x: *mut Fp, buf: *const u8, bufSize: usize) -> usize;
+
+    fn mclBnFp_setInt32(x: *mut Fp, v: i32);
+    fn mclBnFp_setLittleEndian(x: *mut Fp, buf: *const u8, bufSize: usize) -> i32;
+    fn mclBnFp_setLittleEndianMod(x: *mut Fp, buf: *const u8, bufSize: usize) -> i32;
+    fn mclBnFp_setHashOf(x: *mut Fp, buf: *const u8, bufSize: usize) -> i32;
+    fn mclBnFp_setByCSPRNG(x: *mut Fp);
+
+    fn mclBnFp_add(z: *mut Fp, x: *const Fp, y: *const Fp);
+    fn mclBnFp_sub(z: *mut Fp, x: *const Fp, y: *const Fp);
+    fn mclBnFp_neg(y: *mut Fp, x: *const Fp);
+
+    fn mclBnFp_mul(z: *mut Fp, x: *const Fp, y: *const Fp);
+    fn mclBnFp_div(z: *mut Fp, x: *const Fp, y: *const Fp);
+    fn mclBnFp_inv(y: *mut Fp, x: *const Fp);
+    fn mclBnFp_sqr(y: *mut Fp, x: *const Fp);
+    fn mclBnFp_squareRoot(y: *mut Fp, x: *const Fp) -> i32;
+
+	// G1
+    fn mclBnG1_isEqual(x: *const G1, y: *const G1) -> i32;
+    fn mclBnG1_isValid(x: *const G1) -> i32;
+    fn mclBnG1_isZero(x: *const G1) -> i32;
+
+    fn mclBnG1_setStr(x: *mut G1, buf: *const u8, bufSize: usize, ioMode: i32) -> c_int;
+    fn mclBnG1_getStr(buf: *mut u8, maxBufSize: usize, x: *const G1, ioMode: i32) -> usize;
+    fn mclBnG1_serialize(buf: *mut u8, maxBufSize: usize, x: *const G1) -> usize;
+    fn mclBnG1_deserialize(x: *mut G1, buf: *const u8, bufSize: usize) -> usize;
+
+    fn mclBnG1_add(z: *mut G1, x: *const G1, y: *const G1);
+    fn mclBnG1_sub(z: *mut G1, x: *const G1, y: *const G1);
+    fn mclBnG1_neg(y: *mut G1, x: *const G1);
+
+    fn mclBnG1_dbl(y: *mut G1, x: *const G1);
+    fn mclBnG1_mul(z: *mut G1, x: *const G1, y: *const Fr);
+    fn mclBnG1_normalize(y: *mut G1, x: *const G1);
     fn mclBnG1_hashAndMapTo(x: *mut G1, buf: *const u8, bufSize: usize) -> c_int;
+
+	// G2
+    fn mclBnG2_isEqual(x: *const G2, y: *const G2) -> i32;
+    fn mclBnG2_isValid(x: *const G2) -> i32;
+    fn mclBnG2_isZero(x: *const G2) -> i32;
+
+    fn mclBnG2_setStr(x: *mut G2, buf: *const u8, bufSize: usize, ioMode: i32) -> c_int;
+    fn mclBnG2_getStr(buf: *mut u8, maxBufSize: usize, x: *const G2, ioMode: i32) -> usize;
+    fn mclBnG2_serialize(buf: *mut u8, maxBufSize: usize, x: *const G2) -> usize;
+    fn mclBnG2_deserialize(x: *mut G2, buf: *const u8, bufSize: usize) -> usize;
+
+    fn mclBnG2_add(z: *mut G2, x: *const G2, y: *const G2);
+    fn mclBnG2_sub(z: *mut G2, x: *const G2, y: *const G2);
+    fn mclBnG2_neg(y: *mut G2, x: *const G2);
+
+    fn mclBnG2_dbl(y: *mut G2, x: *const G2);
+    fn mclBnG2_mul(z: *mut G2, x: *const G2, y: *const Fr);
+    fn mclBnG2_normalize(y: *mut G2, x: *const G2);
     fn mclBnG2_hashAndMapTo(x: *mut G2, buf: *const u8, bufSize: usize) -> c_int;
 
     fn mclBn_pairing(z: *mut GT, x: *const G1, y: *const G2);
@@ -119,8 +189,13 @@ macro_rules! str_impl {
 }
 
 macro_rules! base_field_impl {
-    ($t:ty, $set_by_csprng_fn:ident, $set_hash_of_fn:ident, $set_little_endian_fn:ident, $set_little_endian_mod_fn:ident) => {
+    ($t:ty, $set_int_fn:ident, $set_by_csprng_fn:ident, $set_hash_of_fn:ident, $set_little_endian_fn:ident, $set_little_endian_mod_fn:ident) => {
         impl $t {
+    pub fn set_int(&mut self, x: i32) {
+        unsafe {
+            $set_int_fn(self, x);
+        }
+    }
             pub fn set_by_csprng(&mut self) {
                 unsafe { $set_by_csprng_fn(self) }
             }
@@ -188,8 +263,8 @@ macro_rules! is_odd_neg_impl {
     };
 }
 
-macro_rules! field_op_impl {
-    ($t:ty, $add_fn:ident, $sub_fn:ident, $mul_fn:ident, $div_fn:ident, $neg_fn:ident, $sqr_fn:ident, $square_root_fn:ident) => {
+macro_rules! add_op_impl {
+    ($t:ty, $add_fn:ident, $sub_fn:ident, $neg_fn:ident) => {
         impl $t {
             pub fn add(z: &mut $t, x: &$t, y: &$t) {
                 unsafe { $add_fn(z, x, y) }
@@ -197,14 +272,24 @@ macro_rules! field_op_impl {
             pub fn sub(z: &mut $t, x: &$t, y: &$t) {
                 unsafe { $sub_fn(z, x, y) }
             }
+            pub fn neg(y: &mut $t, x: &$t) {
+                unsafe { $neg_fn(y, x) }
+            }
+        }
+    };
+}
+
+macro_rules! field_mul_op_impl {
+    ($t:ty, $mul_fn:ident, $div_fn:ident, $inv_fn:ident, $sqr_fn:ident, $square_root_fn:ident) => {
+        impl $t {
             pub fn mul(z: &mut $t, x: &$t, y: &$t) {
                 unsafe { $mul_fn(z, x, y) }
             }
             pub fn div(z: &mut $t, x: &$t, y: &$t) {
                 unsafe { $div_fn(z, x, y) }
             }
-            pub fn neg(y: &mut $t, x: &$t) {
-                unsafe { $neg_fn(y, x) }
+            pub fn inv(y: &mut $t, x: &$t) {
+                unsafe { $inv_fn(y, x) }
             }
             pub fn sqr(y: &mut $t, x: &$t) {
                 unsafe { $sqr_fn(y, x) }
@@ -233,32 +318,37 @@ pub struct Fp2 {
 pub struct Fr {
     d: [u64; MCLBN_FR_UNIT_SIZE],
 }
+is_compare_base_impl![Fr, mclBnFr_isEqual, mclBnFr_isValid, mclBnFr_isZero];
+is_one_impl![Fr, mclBnFr_isOne];
+is_odd_neg_impl![Fr, mclBnFr_isOdd, mclBnFr_isNegative];
+str_impl![Fr, 1024, mclBnFr_getStr, mclBnFr_setStr];
 serialize_impl![
     Fr,
     mclBn_getFrByteSize(),
     mclBnFr_serialize,
     mclBnFr_deserialize
 ];
-str_impl![Fr, 1024, mclBnFr_getStr, mclBnFr_setStr];
-is_compare_base_impl![Fr, mclBnFr_isEqual, mclBnFr_isValid, mclBnFr_isZero];
-is_one_impl![Fr, mclBnFr_isOne];
-is_odd_neg_impl![Fr, mclBnFr_isOdd, mclBnFr_isNegative];
-field_op_impl![
-    Fr,
-    mclBnFr_add,
-    mclBnFr_sub,
-    mclBnFr_mul,
-    mclBnFr_div,
-    mclBnFr_neg,
-    mclBnFr_sqr,
-    mclBnFr_squareRoot
-];
 base_field_impl![
     Fr,
+	mclBnFr_setInt32,
     mclBnFr_setByCSPRNG,
     mclBnFr_setHashOf,
     mclBnFr_setLittleEndian,
     mclBnFr_setLittleEndianMod
+];
+add_op_impl![
+    Fr,
+    mclBnFr_add,
+    mclBnFr_sub,
+    mclBnFr_neg
+];
+field_mul_op_impl![
+    Fr,
+    mclBnFr_mul,
+    mclBnFr_div,
+    mclBnFr_inv,
+    mclBnFr_sqr,
+    mclBnFr_squareRoot
 ];
 
 #[allow(dead_code)]
@@ -299,11 +389,6 @@ impl Fr {
     }
     pub fn clear(&mut self) {
         *self = Fr::zero()
-    }
-    pub fn set_int(&mut self, x: i32) {
-        unsafe {
-            mclBnFr_setInt32(self, x);
-        }
     }
 }
 
