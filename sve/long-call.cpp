718a6af58019ab5a8494c3fc6591619486162447
MITSUNARI Shigeo <herumi@nifty.com>
2020-06-04 11:33:29 +0900

    save register

--- sve/long-call.cpp
+++ sve/long-call.cpp
@@ -2,37 +2,70 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static float *dataG;
-
-void dumpF()
+static void dumpF(const float *x)
 {
+	printf("x=%p\n", x);
 	puts("---");
 	for (int i = 0; i < 16; i++) {
-		printf("%d %f\n", i, dataG[i]);
+		printf("%d %f\n", i, x[i]);
 	}
 	puts("---");
-	exit(1);
 }
 
 struct Code : Xbyak::CodeGenerator {
+	void genDebug(const Xbyak::ZReg& z)
+	{
+		using namespace Xbyak;
+		Label funcL, skipL, exitL;
+		const int xN = 16;
+		const int zN = 24;
+		int stackSize = (xN + 2) * 8 + zN * 64;
+		// save registers
+		stp(x29, x30, pre_ptr(sp, -16));
+		sub(sp, sp, stackSize);
+		for (int i = 0; i < xN; i++) {
+			str(XReg(i), ptr(sp, i * 8));
+		}
+		str(p0, ptr(sp, xN * 8));
+		add(x0, sp, (xN + 2) * 8);
+		for (int i = 0; i < zN; i++) {
+			st1w(ZReg(i).s, p0, ptr(x0));
+			add(x0, x0, 64);
+		}
+		b(skipL);
+		align(16);
+	L(funcL);
+		uint64_t funcAddr = (uint64_t)dumpF;
+		printf("func=%016lx\n", funcAddr);
+		dw(uint32_t(funcAddr));
+		dw(uint32_t(funcAddr >> 32));
+	L(skipL);
+		sub(sp, sp, 64);
+		st1w(z.s, p0, ptr(sp));
+		add(x0, sp, 0);
+		ldr(x1, funcL);
+		blr(x1);
+		add(sp, sp, 64);
+
+		// restore registers
+		add(x0, sp, (xN + 2) * 8);
+		for (int i = 0; i < zN; i++) {
+			ld1w(ZReg(i).s, p0, ptr(x0));
+			add(x0, x0, 64);
+		}
+		for (int i = 0; i < xN; i++) {
+			ldr(XReg(i), ptr(sp, i * 8));
+		}
+		ldr(p0, ptr(sp, xN * 8));
+		add(sp, sp, stackSize);
+		ldp(x29, x30, post_ptr(sp, 16));
+	}
 	Code()
 	{
-		Xbyak::Label funcL, dataL;
 		ptrue(p0.s);
 		ld1w(z0.s, p0, ptr(x0));
-		adr(x0, dataL);
-		st1w(z0.s, p0, ptr(x0));
-		adr(x0, funcL);
-		ldr(x0, ptr(x0));
-		br(x0);
-	L(funcL);
-		int64_t addr = (int64_t)dumpF;
-		dw(uint32_t(addr));
-		dw(uint32_t(addr >> 32));
-	L(dataL);
-		dataG = (float*)getCurr();
-		printf("dumpF=%p\n", dumpF);
-		printf("dataG=%p\n", dataG);
+		genDebug(z0);
+		ret();
 	}
 };
 
@@ -45,5 +78,12 @@ int main()
 	for (int i = 0; i < 16; i++) {
 		a[i] = i * 0.1f;
 	}
+	float b[16];
+	for (int i = 0; i < 16; i++) {
+		b[i] = i * i;
+	}
 	f(a);
+	puts("ok1");
+	f(b);
+	puts("ok2");
 }
