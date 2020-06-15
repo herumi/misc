3ac139e4ea930b3b4af779866cb9809e956c6fc7
MITSUNARI Shigeo <herumi@nifty.com>
2020-06-15 17:19:31 +0900

    test of gelu

---
+++ sve/gelu.cpp
@@ -0,0 +1,144 @@
+#include <stdint.h>
+#include <math.h>
+#include "gelu.hpp"
+#include <cybozu/test.hpp>
+#include <vector>
+
+float g_maxe;
+
+float diff(float x, float y)
+{
+	return std::abs(x - y) / x;
+}
+
+union fi {
+	float f;
+	uint32_t i;
+};
+
+float u2f(uint32_t x)
+{
+	fi fi;
+	fi.i = x;
+	return fi.f;
+}
+
+uint32_t f2u(float x)
+{
+	fi fi;
+	fi.f = x;
+	return fi.i;
+}
+
+float std_gelu(float x)
+{
+	/*
+		0.5x(1 + tanh(sqrt(2/pi)(x + 0.044715x^3)))
+		=0.5x(1 + tanh(sqrt(2/pi)(1 + 0.044715x^2)x))
+		=0.5x(1 + tanh((A + Bx^2)x))
+
+		C = A + Bx^2
+		G = 0.5x(1 + tanh(C))
+		= 0.5x(1 + 1 - 2 / (1 + exp(2C)))
+		= 0.5x(2 - 2 / (1 + exp(2C)))
+		= x(1 - 1/(1+exp(2C)))
+
+		C1 = 2sqrt(2/pi)
+		C2 = 2sqrt(2/pi) * 0.044715
+		C = C1 + C2 x^2
+		G = x(1 - 1/(1 + exp(C x)))
+	*/
+	static const float C1 = u2f(0x3fcc422a);
+	static const float C2 = u2f(0x3d922279);
+	float C = C1 + C2 * x * x;
+	float y = x*(1-1/(1+exp(C*x)));
+
+#if 0
+	float org = 0.5*x*(1 + tanh(sqrt(2/3.14159265358979)*(x + 0.044715 * x * x * x)));
+	float e = fabs(y - org);
+	if (e > 1e-6) {
+		printf("diff %e x=%f y=%f org=%f\n", e, x, y, org);
+	}
+#endif
+	return y;
+}
+
+void std_gelu_v(float *dst, const float *src, size_t n)
+{
+	for (size_t i = 0; i < n; i++) {
+		dst[i] = std_gelu(src[i]);
+	}
+}
+
+template<class F>
+float putDiff(float begin, float end, float step, const F& f, bool doPut = false, float stdf(float) = std::exp)
+{
+	float maxe = 0;
+	float maxx = 0;
+	float maxe2 = 0;
+	float maxx2 = 0;
+	double ave = 0;
+	int aveN = 0;
+	for (float x = begin; x < end; x += step) {
+		float y0 = stdf(x);
+		float y1 = f(x);
+		float e;
+		e = diff(y0, y1);
+		if (doPut) {
+			printf("x=%.2e y0=%.2e(%08x) y1=%.2e(%08x)\n", x, y0, f2u(y0), y1, f2u(y1));
+		}
+		if (e > maxe) {
+			maxe = e;
+			maxx = x;
+		}
+		float e2 = fabs(y0 - y1);
+		if (e2 > maxe2) {
+			maxe2 = e2;
+			maxx2 = x;
+		}
+		ave += e;
+		aveN++;
+	}
+	printf("range [%.2e, %.2e] step=%.2e\n", begin, end, step);
+	printf("maxe =%e (x=%e)\n", maxe, maxx);
+	printf("maxe2=%e (x=%e)\n", maxe2, maxx2);
+	printf("ave=%e\n", ave / aveN);
+	return maxe;
+}
+
+void checkDiff(const float *x, const float *y, size_t n, bool put = false)
+{
+	for (size_t i = 0; i < n; i++) {
+		float d = diff(x[i], y[i]);
+		if (put) {
+			if (d > g_maxe) {
+				printf("err n=%zd, i=%zd x=%e y=%e\n", n, i, x[i], y[i]);
+				exit(1);
+			}
+		} else {
+			CYBOZU_TEST_ASSERT(d <= g_maxe);
+		}
+	}
+}
+
+typedef std::vector<float> Fvec;
+
+CYBOZU_TEST_AUTO(bench)
+{
+	Fvec x, y;
+	size_t n = 1024 * 4;
+	x.resize(n);
+	y.resize(n);
+	for (size_t i = 0; i < n; i++) {
+		x[i] = (i / float(n) - 0.5) * 4;
+	}
+	fmath::gelu_v(&y[0], &x[0], n);
+	for (size_t i = 0; i < n; i++) {
+		float y1 = y[i];
+		float y2 = std_gelu(x[i]);
+		float d = fabs(y1 - y2);
+		if (d > 1e-5) {
+			printf("x=%e ng=%e ok=%e d=%e\n", x[i], y1, y2, d);
+		}
+	}
+}
---
+++ sve/gelu.hpp
@@ -0,0 +1,202 @@
+#pragma once
+/**
+	@author herumi
+	@note modified new BSD license
+	http://opensource.org/licenses/BSD-3-Clause
+*/
+#include <xbyak_aarch64/xbyak_aarch64.h>
+#include <cmath>
+
+namespace fmath {
+
+namespace local {
+
+union fi {
+	float f;
+	uint32_t i;
+};
+
+inline float cvt(uint32_t x)
+{
+	fi fi;
+	fi.i = x;
+	return fi.f;
+}
+
+struct ConstVar {
+	static const size_t expN = 5;
+	float expMin; // exp(expMin) = 0
+	float expMax; // exp(expMax) = inf
+	float log2; // log(2)
+	float log2_e; // log_2(e) = 1 / log2
+	float expCoeff[expN]; // near to 1/(i + 1)!
+	uint32_t geluC1;
+	uint32_t geluC2;
+	void init()
+	{
+		expMin = cvt(0xc2aeac50);
+		expMax = cvt(0x42b17218);
+		log2 = std::log(2.0f);
+		log2_e = 1.0f / log2;
+		// maxe=1.938668e-06
+		const uint32_t expTbl[expN] = {
+			0x3f800000,
+			0x3effff12,
+			0x3e2aaa56,
+			0x3d2b89cc,
+			0x3c091331,
+		};
+		for (size_t i = 0; i < expN; i++) {
+			expCoeff[i] = cvt(expTbl[i]);
+		}
+		geluC1 = 0x3fcc422a;
+		geluC2 = 0x3d922279;
+	}
+};
+
+struct Code : public Xbyak::CodeGenerator {
+	typedef Xbyak::ZReg ZReg;
+	typedef Xbyak::PReg PReg;
+	ConstVar *constVar;
+	typedef void (*VecFunc)(float *dst, const float *src, size_t n);
+	VecFunc gelu_v;
+	Code()
+		: Xbyak::CodeGenerator(4096 * 2)
+		, gelu_v(0)
+	{
+		size_t dataSize = sizeof(ConstVar);
+		dataSize = (dataSize + 4095) & ~size_t(4095);
+		Xbyak::Label constVarL = L();
+		constVar = (ConstVar*)getCode();
+		constVar->init();
+		setSize(dataSize / 4);
+		gelu_v = getCurr<VecFunc>();
+		genGelu(constVarL);
+		ready();
+	}
+	// tz0 = exp(tz0)
+	// use tz0, tz1, tz2, tz3
+	void gen1Gelu(const PReg& p, const ZReg& tz0, const ZReg& tz1, const ZReg& tz2, const ZReg& tz3, const ZReg& expMin, const ZReg& expMax, const ZReg& log2, const ZReg& log2_e, const ZReg expCoeff[5], const ZReg& C1, const ZReg& C2)
+	{
+		// fmad(dn, m, a) ; dn = a + dn * m
+		// x^2
+		fmul(tz3.s, tz0.s, tz0.s);
+		fmad(tz3.s, p, C2.s, C1.s);
+		// Cx
+		fmul(tz3.s, tz3.s, tz0.s);
+
+		// exp(Cx)
+		fmin(tz3.s, p, expMax.s);
+		fmax(tz3.s, p, expMin.s);
+		fmul(tz3.s, tz3.s, log2_e.s);
+		frintn(tz2.s, p, tz3.s); // rounding : float -> float
+		fcvtzs(tz1.s, p, tz2.s); // float -> int
+		fsub(tz2.s, tz3.s, tz2.s);
+		fmul(tz2.s, tz2.s, log2.s);
+		movprfx(tz3.s, p, expCoeff[4].s);
+		fmad(tz3.s, p, tz2.s, expCoeff[3].s);
+		fmad(tz3.s, p, tz2.s, expCoeff[2].s);
+		fmad(tz3.s, p, tz2.s, expCoeff[1].s);
+		fmad(tz3.s, p, tz2.s, expCoeff[0].s);
+		fmad(tz3.s, p, tz2.s, expCoeff[0].s);
+		fscale(tz3.s, p, tz1.s); // tz3 *= 2^tz1
+
+		// 1 + exp(Cx)
+		fadd(tz3.s, tz3.s, expCoeff[0].s);
+		// 1 / (1 + exp(Cx))
+		frecpe(tz1.s, tz3.s);
+		frecps(tz3.s, tz3.s, tz1.s);
+		fmul(tz3.s, tz3.s, tz1.s);
+		// 1 - 1/(1 + exp(Cx))
+		fsub(tz3.s, expCoeff[0].s, tz3.s);
+		// G(x)
+		fmul(tz0.s, tz0.s, tz3.s);
+	}
+	// gelu_v(float *dst, const float *src, size_t n);
+	void genGelu(const Xbyak::Label& constVarL)
+	{
+		using namespace Xbyak;
+		const XReg& dst = x0;
+		const XReg& src = x1;
+		const XReg& n = x2;
+
+		// setup constant
+		const ZReg& expMin = z4;
+		const ZReg& expMax = z5;
+		const ZReg& log2 = z6;
+		const ZReg& log2_e = z7;
+		const ZReg& geluC1 = z8;
+		const ZReg& geluC2 = z9;
+		const ZReg expCoeff[] = {
+			z10, z11, z12, z13, z14,
+		};
+		const size_t saveN = 14 - 8 + 1; /* z8..z14 */
+		const size_t adj = saveN / 2;
+		sub(sp, sp, saveN * 64);
+		add(x3, sp, adj * 64);
+		ptrue(p0.s);
+		for (size_t i = 0; i < saveN; i++) {
+			st1w(ZReg(i + 8).s, p0, ptr(x3, int(i - adj)));
+		}
+
+		adr(x3, constVarL);
+		ldr(w4, ptr(x3, (uint32_t)offsetof(ConstVar, expMin)));
+		cpy(expMin.s, p0/T_z, w4);
+		ldr(w4, ptr(x3, (uint32_t)offsetof(ConstVar, expMax)));
+		cpy(expMax.s, p0/T_z, w4);
+		ldr(w4, ptr(x3, (uint32_t)offsetof(ConstVar, log2)));
+		cpy(log2.s, p0/T_z, w4);
+		ldr(w4, ptr(x3, (uint32_t)offsetof(ConstVar, log2_e)));
+		cpy(log2_e.s, p0/T_z, w4);
+		ldr(w4, ptr(x3, (uint32_t)offsetof(ConstVar, geluC1)));
+		cpy(geluC1.s, p0/T_z, w4);
+		ldr(w4, ptr(x3, (uint32_t)offsetof(ConstVar, geluC2)));
+		cpy(geluC2.s, p0/T_z, w4);
+		for (size_t i = 0; i < ConstVar::expN; i++) {
+			ldr(w4, ptr(x3, uint32_t(offsetof(ConstVar, expCoeff[0]) + sizeof(float) * i)));
+			cpy(expCoeff[i].s, p0/T_z, w4);
+		}
+		Label skip;
+		b(skip);
+	Label lp = L();
+		ld1w(z0.s, p0/T_z, ptr(src));
+		add(src, src, 64);
+		gen1Gelu(p0, z0, z1, z2, z3, expMin, expMax, log2, log2_e, expCoeff, geluC1, geluC2);
+		st1w(z0.s, p0, ptr(dst));
+		add(dst, dst, 64);
+		sub(n, n, 16);
+	L(skip);
+		cmp(n, 16);
+		bge(lp);
+
+		mov(x3, 0);
+		whilelt(p1.s, x3, n);
+		ld1w(z0.s, p1/T_z, ptr(src));
+		gen1Gelu(p1, z0, z1, z2, z3, expMin, expMax, log2, log2_e, expCoeff, geluC1, geluC2);
+		st1w(z0.s, p1, ptr(dst));
+
+		add(x3, sp, adj * 64);
+		for (size_t i = 0; i < saveN; i++) {
+			ld1w(ZReg(i + 8).s, p0, ptr(x3, int(i - adj)));
+		}
+		add(sp, sp, saveN * 64);
+		ret();
+	}
+};
+
+template<size_t dummy = 0>
+struct Inst {
+	static const Code code;
+};
+
+template<size_t dummy>
+alignas(32) const Code Inst<dummy>::code;
+
+} // fmath::local
+
+inline void gelu_v(float *dst, const float *src, size_t n)
+{
+	local::Inst<>::code.gelu_v(dst, src, n);
+}
+
+} // fmath2
