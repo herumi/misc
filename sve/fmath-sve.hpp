2e3de3aaffefadad5a022eee0de5da30402efd46
MITSUNARI Shigeo <herumi@nifty.com>
2020-07-08 14:09:52 +0900

    remove unused code

--- sve/fmath-sve.hpp
+++ sve/fmath-sve.hpp
@@ -32,25 +32,12 @@ struct ConstVar {
 	float log2_e; // log_2(e) = 1 / log2
 	float expCoeff[expN]; // near to 1/(i + 1)!
 	//
-	float log1p5; // log(1.5)
-	float f2div3; // 2/3
-	float logCoeff[logN];
 	void init()
 	{
 		expMin = cvt(0xc2aeac50);
 		expMax = cvt(0x42b17218);
 		log2 = std::log(2.0f);
 		log2_e = 1.0f / log2;
-		log1p5 = std::log(1.5f);
-		f2div3 = 2.0f/3;
-#if 0
-		// maxe=4.888831e-06
-		float z = 1;
-		for (size_t i = 0; i < expN; i++) {
-			expCoeff[i] = z;
-			z /= (i + 2);
-		}
-#else
 		// maxe=1.938668e-06
 		const uint32_t expTbl[expN] = {
 			0x3f800000,
@@ -61,21 +48,6 @@ struct ConstVar {
 		};
 		for (size_t i = 0; i < expN; i++) {
 			expCoeff[i] = cvt(expTbl[i]);
-		}
-#endif
-		const float logTbl[logN] = {
-			 1.0, // must be 1
-			-0.49999985195974875681242,
-			 0.33333220526061677705782,
-			-0.25004206220486390058000,
-			 0.20010985747510067100077,
-			-0.16481566812093889672203,
-			 0.13988269735629330763020,
-			-0.15049504706005165294002,
-			 0.14095711402233803479921,
-		};
-		for (size_t i = 0; i < logN; i++) {
-			logCoeff[i] = logTbl[i];
 		}
 	}
 };
@@ -114,11 +86,6 @@ struct Code : public Xbyak::CodeGenerator {
 		align(16);
 		tanhf_v = getCurr<VecFunc>();
 		genTanhSVE(constVarL);
-#if 0
-		align(16);
-		logf_v = getCurr<VecFunc>();
-		genLogAVX512(constVarL);
-#endif
 		ready();
 	}
 	// tz0 = exp(tz0)
@@ -415,104 +382,6 @@ struct Code : public Xbyak::CodeGenerator {
 
 		ret();
 	}
-#if 0
-	// zm0 = log(zm0)
-	// use zm0, zm1, zm2
-	void genLogOneAVX512(const Zmm& i127shl23, const Zmm& x7fffff, const Zmm& log2, const Zmm& log1p5, const Zmm& f2div3, const Zmm *logCoeff)
-	{
-		vpsubd(zm1, zm0, i127shl23);
-		vpsrad(zm1, zm1, 23); // e
-		vcvtdq2ps(zm1, zm1); // float(e)
-		vpandd(zm0, zm0, x7fffff);
-		vpord(zm0, zm0, i127shl23); // y
-
-		vfmsub213ps(zm0, f2div3, logCoeff[0]); // a
-		vfmadd213ps(zm1, log2, log1p5); // e
-
-		int logN = ConstVar::logN;
-		vmovaps(zm2, logCoeff[logN - 1]);
-		for (int i = logN - 2; i >= 0; i--) {
-			vfmadd213ps(zm2, zm0, logCoeff[i]);
-		}
-		vfmadd213ps(zm0, zm2, zm1);
-	}
-	// log_v(float *dst, const float *src, size_t n);
-	void genLogAVX512(const Xbyak::Label& constVarL)
-	{
-		const int keepRegN = 11;
-		using namespace Xbyak;
-		util::StackFrame sf(this, 3, util::UseRCX, 64 * keepRegN);
-		const Reg64& dst = sf.p[0];
-		const Reg64& src = sf.p[1];
-		const Reg64& n = sf.p[2];
-
-		// prolog
-#ifdef XBYAK64_WIN
-		vmovups(ptr[rsp + 64 * 0], zm6);
-		vmovups(ptr[rsp + 64 * 1], zm7);
-#endif
-		for (int i = 2; i < keepRegN; i++) {
-			vmovups(ptr[rsp + 64 * i], Zmm(i + 6));
-		}
-
-		// setup constant
-		const Zmm& i127shl23 = zmm3;
-		const Zmm& x7fffff = zmm4;
-		const Zmm& log2 = zmm5;
-		const Zmm& log1p5 = zmm6;
-		const Zmm& f2div3 = zmm7;
-		const Zmm logCoeff[ConstVar::logN] = {
-			zm8, zm9, zm10, zm11, zm12, zm13, zm14, zm15, zm16
-		};
-		mov(eax, 127 << 23);
-		vpbroadcastd(i127shl23, eax);
-		mov(eax, 0x7fffff);
-		vpbroadcastd(x7fffff, eax);
-
-		lea(rax, ptr[rip+constVarL]);
-		vbroadcastss(log2, ptr[rax + offsetof(ConstVar, log2)]);
-		vbroadcastss(log1p5, ptr[rax + offsetof(ConstVar, log1p5)]);
-		vbroadcastss(f2div3, ptr[rax + offsetof(ConstVar, f2div3)]);
-		for (size_t i = 0; i < ConstVar::logN; i++) {
-			vbroadcastss(logCoeff[i], ptr[rax + offsetof(ConstVar, logCoeff[0]) + sizeof(float) * i]);
-		}
-
-		// main loop
-		Label mod16, exit;
-		mov(ecx, n);
-		and_(n, ~15);
-		jz(mod16, T_NEAR);
-	Label lp = L();
-		vmovups(zm0, ptr[src]);
-		add(src, 64);
-		genLogOneAVX512(i127shl23, x7fffff, log2, log1p5, f2div3, logCoeff);
-		vmovups(ptr[dst], zm0);
-
-		add(dst, 64);
-		sub(n, 16);
-		jnz(lp);
-	L(mod16);
-		and_(ecx, 15);
-		jz(exit, T_NEAR);
-		mov(eax, 1);
-		shl(eax, cl);
-		sub(eax, 1);
-		kmovd(k1, eax);
-		vmovups(zm0|k1|T_z, ptr[src]);
-		genLogOneAVX512(i127shl23, x7fffff, log2, log1p5, f2div3, logCoeff);
-		vmovups(ptr[dst]|k1, zm0|k1);
-	L(exit);
-
-		// epilog
-#ifdef XBYAK64_WIN
-		vmovups(zm6, ptr[rsp + 64 * 0]);
-		vmovups(zm7, ptr[rsp + 64 * 1]);
-#endif
-		for (int i = 2; i < keepRegN; i++) {
-			vmovups(Zmm(i + 6), ptr[rsp + 64 * i]);
-		}
-	}
-#endif
 };
 
 template<size_t dummy = 0>
@@ -534,12 +403,5 @@ inline void tanhf_v(float *dst, const float *src, size_t n)
 {
 	local::Inst<>::code.tanhf_v(dst, src, n);
 }
-
-#if 0
-inline void logf_v(float *dst, const float *src, size_t n)
-{
-	local::Inst<>::code.logf_v(dst, src, n);
-}
-#endif
 
 } // fmath2
