730da8f1a213de867a0707abee22d6932209a097
MITSUNARI Shigeo <herumi@nifty.com>
2020-07-08 10:05:59 +0900

    remove warning

--- sve/fexpa-test.cpp
+++ sve/fexpa-test.cpp
@@ -8,9 +8,9 @@ union fi {
 
 float expC(float x)
 {
-	static const float log2_e = 1 / log(2.0f);
+	static const float log2_e = float(1 / log(2.0));
 	float y = x * log2_e;
-	float z = y - 1;
+	float z = y;
 	int n = (int)floor(z);
 	float a = z - n; // 0 <= a < 1
 	float b = 1 + a; // 1 <= b < 2, y = n + b
@@ -18,15 +18,15 @@ float expC(float x)
 	fi.f = b;
 	fi.i >>= 17;
 	float c = fexpaEmu(fi.f);
-	return pow(2, n+1) * c;
+	return powf(2, (float)n) * c;
 }
 
 int main()
 {
 	for (float x = 0; x < 2; x += 0.1) {
-		float y1 = exp(x);
+		float y1 = expf(x);
 		float y2 = expC(x);
-		float e = fabs(y1 - y2);
+		float e = fabsf(y1 - y2);
 		printf("x=%f, y1=%f, y2=%f, diff=%e\n", x, y1, y2, e);
 	}
 }
