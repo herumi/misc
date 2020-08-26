902b86d4e9ff5bf60ae16de573c982c3c0b5fa2b
MITSUNARI Shigeo <herumi@nifty.com>
2020-08-26 15:04:08 +0900

    use MT

--- cpp/sample/map_test.cpp
+++ cpp/sample/map_test.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cybozu/xorshift.hpp>
+#include <random>
 #include <map>
 
 struct G {
@@ -31,6 +31,8 @@ struct G {
 		printf("copy_assign=%d mov_assign=%d\n", copy_assign, mov_assign);
 		printf("1 lt=%d eq=%d\n", lt1, eq1);
 		printf("2 lt=%d eq=%d\n", lt2, eq2);
+		printf("total=%d\n", lt1 + eq1 + lt2 + eq2);
+		puts("");
 	}
 } g;
 
@@ -53,15 +55,14 @@ struct A {};
 
 using Map = std::map<X, A, std::less<>>;
 
-template<class M, class F>
-void test(const F& f)
+template<class F>
+void test(const F& f, int N, int R)
 {
-	cybozu::XorShift rg;
-    M m;
-	const int C = 65537;
+	std::mt19937 rg;
+    Map m;
 	g.clear();
-	for (int i = 0; i < 1000; i++) {
-		int v = rg.get32() % C;
+	for (int i = 0; i < N; i++) {
+		int v = rg() % R;
 		f(m, v);
 	}
 	g.put();
@@ -70,13 +71,6 @@ void test(const F& f)
 void insert_if_absent1(Map& m, int v)
 {
 	m.try_emplace(X(v));
-}
-
-void insert_if_absent2(Map& m, int v)
-{
-    Map::iterator it = m.find(X(v));
-    if (it != m.end()) return;
-    m.try_emplace(X(v));
 }
 
 void insert_if_absent3(Map& m, int v)
@@ -88,31 +82,12 @@ void insert_if_absent3(Map& m, int v)
     m.emplace_hint(it, v, A());
 }
 
-struct B {
-	struct A a;
-	bool copy;
-	B()
-		: a()
-		, copy(false)
-	{
-	}
-	B(const B&) { copy = false; }
-	B(B&&) { copy = false; }
-	void operator=(const B&) { copy = false; }
-	void operator=(B&&) { copy = false; }
-};
-
-using Map2 = std::map<X, B, std::less<>>;
-
-void insert_if_absent4(Map2& m, int v)
-{
-    m.emplace(v, B());
-}
-
 int main()
 {
-	test<Map>(insert_if_absent1);
-	test<Map>(insert_if_absent2);
-	test<Map>(insert_if_absent3);
-	test<Map2>(insert_if_absent4);
+	puts("test1");
+	test(insert_if_absent1, 10000, 257);
+	test(insert_if_absent3, 10000, 257);
+	puts("test2");
+	test(insert_if_absent1, 1000, 65537);
+	test(insert_if_absent3, 1000, 65537);
 }
