5b432eaa0834948cdc474687c1e36a975d8044e7
MITSUNARI Shigeo <herumi@nifty.com>
2020-11-09 15:20:42 +0900

    use EXPORT_NAME

--- webpack/test2/src/index.js
+++ webpack/test2/src/index.js
@@ -1,14 +1,5 @@
-exports.init = function() {
-    return new Promise(resolve => {
-//      const wasmCode = Buffer.from(data.wasmBase64, 'base64')
-      const js = require(`./add.js`)
-      const Module = {
-//        wasmBinary: wasmBinary,
-      }
-      js(Module)
-        .then(_mod => {
-          exports.mod = _mod
-          resolve()
-        })
-    })
+const createModule = require(`./add.js`)
+exports.init = async() => {
+  console.log('exports.init')
+  exports.mod = await createModule()
 }
