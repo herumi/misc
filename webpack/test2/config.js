3d4422e6a6c34345d9aa4941dbb1c63c5dbdbb34
MITSUNARI Shigeo <herumi@nifty.com>
2020-11-10 11:23:11 +0900

    make library

--- webpack/test2/config.js
+++ webpack/test2/config.js
@@ -1,7 +1,11 @@
 module.exports = {
+//  mode: 'development',
   entry: './src/index.js',
   output: {
     path: __dirname + '/',
+    library: 'bls',
+    libraryTarget: 'umd',
     filename: 'index.js'
-  }
+  },
+  target: "node"
 }
