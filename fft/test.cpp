6ca48a4a09660f56b81f94e4c7ab8e02c18866e5
MITSUNARI Shigeo <herumi@nifty.com>
2020-12-24 17:38:30 +0900

    use mkl_fft

--- fft/test.cpp
+++ fft/test.cpp
@@ -1,16 +1,51 @@
 #include <spqlios-fft.h>
 #include <stdio.h>
+#include <mkl_dfti.h>
 
-int main()
+const int N = 16;
+
+void init(double *r)
 {
-	const int N = 16;
+	for (int i = 0; i < N; i++) {
+		r[i] = i + 1;
+	}
+}
+
+void put(const double *r, const char *msg = 0)
+{
+	if (msg) printf("%s\n", msg);
+	for (int i = 0; i < N; i++) {
+		printf("%8.4f", r[i]);
+		if ((i % 4) == 3) printf("\n");
+	}
+}
+
+void spqlios()
+{
+	FFT_Processor_Spqlios spq(N);
+	double r[N];
+	init(r);
+	put(r, "init");
+	const double c = 2 / double(N);
+	for (int i = 0; i < N; i++) {
+		spq.real_inout_direct[i] = r[i];// * c;
+	}
+	fft(spq.tables_direct, spq.real_inout_direct);
+	put(spq.real_inout_direct, "fft");
+	for (int i = 0; i < N; i++) {
+		spq.real_inout_rev[i] = spq.real_inout_direct[i];
+	}
+	ifft(spq.tables_reverse, spq.real_inout_rev);
+	put(spq.real_inout_rev, "ifft");
+}
+#if 0
+void spqlios()
+{
 	FFT_Processor_Spqlios spq(N);
 	uint32_t a[N];
 	uint32_t b[N];
 	double r[N];
-	for (int i = 0; i < N; i++) {
-		a[i] = 8;
-	}
+	init(a);
 	spq.execute_reverse_torus32(r, a);
 	puts("r");
 	for (int i = 0; i < N; i++) {
@@ -45,4 +80,34 @@ int main()
 		if ((i % 4) == 3) printf("\n");
 	}
 	puts("--- outer ---");
+}
+#endif
+
+void fftw3()
+{
+	double r[N + 2];
+	init(r);
+	put(r, "init");
+	DFTI_DESCRIPTOR_HANDLE hdl, hdl2;
+	MKL_LONG status;
+	status = DftiCreateDescriptor(&hdl, DFTI_DOUBLE, DFTI_REAL, 1, N);
+	if (status) printf("status=%s\n", DftiErrorMessage(status));
+	status = DftiCommitDescriptor(hdl);
+	if (status) printf("status=%s\n", DftiErrorMessage(status));
+
+
+	status = DftiComputeForward(hdl, r);
+	if (status) printf("status=%s\n", DftiErrorMessage(status));
+	put(r, "forward");
+	status = DftiComputeBackward(hdl, r);
+	if (status) printf("status=%s\n", DftiErrorMessage(status));
+	put(r, "backward");
+	status = DftiFreeDescriptor(&hdl);
+	if (status) printf("status=%s\n", DftiErrorMessage(status));
+}
+
+int main()
+{
+	spqlios();
+	fftw3();
 }
