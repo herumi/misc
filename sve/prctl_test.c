2e76b3c4d0301ef9c0a29bb50029c6dc8827c9f6
MITSUNARI Shigeo <herumi@nifty.com>
2021-01-07 12:59:53 +0900

    check macro

--- sve/prctl_test.c
+++ sve/prctl_test.c
@@ -11,8 +11,12 @@ sve len=00000100
 use option -march=armv8.2-a+sve
 */
 
+#if defined(__ARM_FEATURE_SVE) && !defined(__CLANG_FUJITSU)
+	#define USE_INLINE_SVE 1
+#endif
+
 int
-#if 0
+#ifndef USE_INLINE_SVE
 #ifdef __clang__
 __attribute__((optnone))
 #else
@@ -22,13 +26,26 @@ __attribute__((optimize("O0")))
 getLen()
 {
   uint64_t x = 0;
+#ifdef USE_INLINE_SVE
   asm __volatile__("cntb %[x]" : [ x ] "=r"(x));
-//  asm __volatile__(".inst 0x0420e3e0":"=r"(x));
+#else
+  asm __volatile__(".inst 0x0420e3e0":"=r"(x));
+#endif
   return (int)x;
 }
 
 int main()
 {
+#ifdef __ARM_FEATURE_SVE
+  puts("__ARM_FEATURE_SVE is defined");
+#else
+  puts("__ARM_FEATURE_SVE is not defined");
+#endif
+#ifdef USE_INLINE_SVE
+  puts("use inline sve");
+#else
+  puts("not use inline sve");
+#endif
   int x = prctl(PR_SVE_GET_VL);
   x = prctl(PR_SVE_GET_VL);
   printf("sve len=%08x\n", x);
