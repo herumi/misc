4c41d253acfe8edf7c849284102793179d86a6c5
MITSUNARI Shigeo <herumi@nifty.com>
2021-01-12 17:33:48 +0900

    test

--- blst/bench.cpp
+++ blst/bench.cpp
@@ -3,10 +3,14 @@
 
 #define MCL_USE_VINT
 #define CYBOZU_DONT_USE_OPENSSL
+#define MCL_MAX_BIT_SIZE 384
+#define MCL_STATIC_CODE
 #define PUT(x) std::cout << #x "=" << x << std::endl;
 cybozu::CpuClock clk;
 #include <mcl/bls12_381.hpp>
 #include <cybozu/xorshift.hpp>
+
+mcl::bn::GT g_blst_e;
 
 void put(const char *msg, const void *buf, size_t n)
 {
@@ -72,7 +76,9 @@ void blst_bench()
 	
 	PT e(Q, P);
 	e.final_exp();
-	put("e", e);
+	g_blst_e = *(mcl::bn::GT*)&e;
+//	put("e", e);
+	puts("--- blst ---");
 	CYBOZU_BENCH_C("pairing", 1000, pairing, P, Q);
 	CYBOZU_BENCH_C("ML", 1000, PT, Q, P);
 	CYBOZU_BENCH_C("FE", 1000, e.final_exp);
@@ -89,37 +95,37 @@ void blst_bench()
 	}
 }
 
-void mcl_bench()
+void mcl_bench_sub()
 {
 	using namespace mcl::bn;
-	initPairing(mcl::BLS12_381);
 	G1 P;
 	G2 Q;
-puts("P");
 	P.setStr(
 	"1 3685416753713387016781088315183077757961620795782546409894578378688607592378376318836054947676345821548104185464507"
 	" 1339506544944476473020471379941921221584933875938349620426543736416511423956333506472724655353366534992391756441569",
 	10);
-puts("Q");
 	Q.setStr(
 	"1 352701069587466618187139116011060144890029952792775240219908644239793785735715026873347600343865175952761926303160"
 	" 3059144344244213709971259814753781636986470325476647558659373206291635324768958432433509563104347017837885763365758"
 	" 1985150602287291935568054521177171638300868978215655730859378665066344726373823718423869104263333984641494340347905"
 	" 927553665492332455747201965776037880757740193453592970025027978793976877002675564980949289727957565575433344219582",
 	10);
-
+#if 0
 	printf("P=%s\n", P.getStr(16).c_str());
 	printf("Q=%s\n", Q.getStr(16).c_str());
-	GT e;
+#endif
+	GT e, e2;
 	millerLoop(e, P, Q);
 	finalExp(e, e);
-	printf("e\n%s\n", e.getStr(16).c_str());
+//	printf("e\n%s\n", e.getStr(16).c_str());
+	printf("check %s\n", e == g_blst_e ? "ok" : "ng");
 	CYBOZU_BENCH_C("pairing", 1000, pairing, e, P, Q);
 	CYBOZU_BENCH_C("ML", 1000, millerLoop, e, P, Q);
 	CYBOZU_BENCH_C("FE", 1000, finalExp, e, e);
 	{
 		Fp a = e.a.a.a;
 		Fp b = e.a.a.b;
+		CYBOZU_BENCH_C("fp::mul", 10000, Fp::mul, a, a, b);
 		CYBOZU_BENCH_C("fp::mul", 10000, Fp::mul, a, a, b);
 	}
 	{
@@ -130,8 +136,23 @@ puts("Q");
 	}
 }
 
+void mcl_bench()
+{
+	puts("--- use mcl ---");
+	mcl_bench_sub();
+
+	puts("--- use raw blst ---");
+	using namespace mcl::bn;
+	Fp::mul = (void (*)(Fp&, const Fp&, const Fp&))blst::blst_fp_mul;
+	Fp::sqr = (void (*)(Fp&, const Fp&))blst::blst_fp_sqr;
+	Fp2::mul = (void (*)(Fp2&, const Fp2&, const Fp2&))blst::blst_fp2_mul;
+	Fp2::sqr = (void (*)(Fp2&, const Fp2&))blst::blst_fp2_sqr;
+	mcl_bench_sub();
+}
+
 int main()
 {
+	mcl::bn::initPairing(mcl::BLS12_381);
 	blst_bench();
 	mcl_bench();
 }
