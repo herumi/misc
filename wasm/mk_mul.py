882d595918c969b90fe7d0e7b516b0b18230286d
MITSUNARI Shigeo <herumi@nifty.com>
2021-02-24 09:18:07 +0900

    fix mulT

--- wasm/mk_mul.py
+++ wasm/mk_mul.py
@@ -1,12 +1,15 @@
 def mk_mul(N):
-	print(f'void mulT{N}(uint32_t *z, const uint32_t *x, const uint32_t *y)')
+	print('template<>')
+	print(f'void mulT<{N}>(uint32_t *z, const uint32_t *x, const uint32_t *y)')
 	print(f'{{')
 	print(f'	uint64_t v, L, H;')
-	print(f'	v = x[0] * y[0];')
+	print(f'	v = uint64_t(x[0]) * y[0];')
 	print(f'	z[0] = uint32_t(v);')
+	print(f'	L = 0;')
 	print(f'	H = v >> 32;')
 	for d in range(1, N * 2):
-		print(f'	L = H;')
+		print(f'	L = L >> 32;')
+		print(f'	L += H;')
 		print(f'	H = 0;')
 		for i in range(min(d+1, N)):
 			if d - i >= N:
@@ -18,4 +21,9 @@ def mk_mul(N):
 	print(f'}}')
 
 
-mk_mul(2)
+print("""
+template<size_t N>
+void mulT(uint32_t *z, const uint32_t *x, const uint32_t *y);""")
+
+for i in [1, 2, 3]:
+	mk_mul(i)
--- wasm/mul_test.cpp
+++ wasm/mul_test.cpp
@@ -11,22 +11,31 @@ void setRand(uint32_t *x, size_t n, RG& rg)
 	}
 }
 
-CYBOZU_TEST_AUTO(mul2)
+template<size_t N>
+void testMul()
 {
-	const size_t N = 2;
+	printf("testMul%zd\n", N);
 	mpz_class mz, mx, my;
 	uint32_t x[N], y[N], z[N * 2];
 	cybozu::XorShift rg;
 	std::cout << std::hex;
-	for (int i = 0; i < 1; i++) {
+	for (int i = 0; i < 10; i++) {
 		setRand(x, N, rg);
 		setRand(y, N, rg);
 		mcl::gmp::setArray(mx, x, N);
 		mcl::gmp::setArray(my, y, N);
 		mz = mx * my;
-		mulT2(z, x, y);
+		mulT<N>(z, x, y);
 		uint32_t ok[N * 2];
 		mcl::gmp::getArray(ok, N * 2, mz);
 		CYBOZU_TEST_EQUAL_ARRAY(z, ok, N * 2);
 	}
 }
+
+CYBOZU_TEST_AUTO(mulTest)
+{
+	testMul<1>();
+	testMul<2>();
+	testMul<3>();
+}
+
