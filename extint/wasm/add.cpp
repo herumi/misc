b4789175247a479840a0512016c529c07c54bcff
MITSUNARI Shigeo <herumi@nifty.com>
2021-03-16 11:16:13 +0900

    add bench

--- extint/wasm/add.cpp
+++ extint/wasm/add.cpp
@@ -1,16 +1,56 @@
 #include "api.h"
 #include "../exti.hpp"
 
-inline exti256_t* cast256(int *x) { return (exti256_t*)x; }
-inline const exti256_t* cast256(const int *x) { return (const exti256_t*)x; }
-inline exti512_t* cast512(int *x) { return (exti512_t*)x; }
-inline const exti512_t* cast512(const int *x) { return (const exti512_t*)x; }
+inline exti256_t* cast256(uint32_t *x) { return (exti256_t*)x; }
+inline const exti256_t* cast256(const uint32_t *x) { return (const exti256_t*)x; }
+inline exti512_t* cast512(uint32_t *x) { return (exti512_t*)x; }
+inline const exti512_t* cast512(const uint32_t *x) { return (const exti512_t*)x; }
+
+template<uint32_t N>
+void addT64(uint64_t *z, const uint64_t *x, const uint64_t *y)
+{
+	uint32_t c = 0;
+	for (uint32_t i = 0; i < N; i++) {
+		uint64_t xc = x[i] + c;
+		if (xc < c) {
+			// x[i] = Unit(-1) and c = 1
+			z[i] = y[i];
+		} else {
+			xc += y[i];
+			c = y[i] > xc ? 1 : 0;
+			z[i] = xc;
+		}
+	}
+}
+
+template<uint32_t N>
+uint32_t addT32(uint32_t z[N], const uint32_t x[N], const uint32_t y[N])
+{
+	uint32_t c = 0;
+	for (uint32_t i = 0; i < N; i++) {
+		uint64_t v = uint64_t(x[i]) + y[i] + c;
+		z[i] = uint32_t(v);
+		c = uint32_t(v >> 32);
+	}
+	return c;
+}
+
 
 extern "C" {
 
-API void add256(int *z, const int *x, const int *y)
+API void add256_extInt(uint32_t *z, const uint32_t *x, const uint32_t *y)
 {
 	*cast256(z) = *cast256(x) + *cast256(y);
+}
+
+API void add256_u32(uint32_t *z, const uint32_t *x, const uint32_t *y)
+{
+	addT32<8>(z, x, y);
+}
+
+API void add256_u64(uint64_t *z, const uint64_t *x, const uint64_t *y)
+{
+	addT64<4>(z, x, y);
 }
 
 }
--- extint/wasm/test.js
+++ extint/wasm/test.js
@@ -12,6 +12,17 @@
       memory: memory,
     }
   }
+  const { performance } = require('perf_hooks')
+  const bench = (label, count, func) => {
+    const start = performance.now()
+    for (let i = 0; i < count; i++) {
+      func()
+    }
+    const end = performance.now()
+    const t = (end - start) / count
+    const roundTime = (Math.round(t * 1e9)) / 1000
+    console.log(label + ' ' + roundTime + ' nsec')
+  }
   WebAssembly.instantiate(buf, imports).then(
     ret => {
       exports.ret = ret
@@ -22,23 +33,43 @@
       exports.mem = mem
       const u32 = new Uint32Array(mem)
       const N = 8
-      const INT_SIZE = N * 4
+      const BYTE_SIZE = N * 4
       const xPos = 0
-      const yPos = INT_SIZE
-      const zPos = INT_SIZE * 2
+      const yPos = BYTE_SIZE
+      const z1Pos = BYTE_SIZE * 2
+      const z2Pos = BYTE_SIZE * 3
+      const z3Pos = BYTE_SIZE * 4
+      const dump = (msg, pos) => {
+        console.log(`msg=${msg}, pos=${pos}`)
+        for (let i = 0; i < N; i += 4) {
+          console.log(`${u32[pos/4+i]} ${u32[pos/4+i+1]} ${u32[pos/4+i+2]} ${u32[pos/4+i+3]}`)
+        }
+      }
+      const cmpArray = (pos1, pos2) => {
+        for (let i = 0; i < N; i++) {
+          const v1 = u32[pos1/4+i]
+          const v2 = u32[pos2/4+i]
+          if (v1 !== v2) {
+            console.log(`QQQ i=${i} v1=${v1} v2=${v2}`)
+          }
+        }
+      }
       for (let i = 0; i < N; i++) {
-        u32[i] = i + 1
-        u32[N + i] = i + 10
+        u32[i] = i * 2147483643 + 1
+        u32[N + i] = i * 2147483642 + 11
       }
+      dump('x', xPos)
+      dump('y', yPos)
       console.log('before')
-      for (let i = 0; i < N * 3; i++) {
-        console.log(`mem[${i}]=${u32[i]}`)
-      }
-      mod.add256(zPos, yPos, xPos)
+      mod.add256_extInt(z1Pos, yPos, xPos)
+      mod.add256_u32(z2Pos, yPos, xPos)
+      mod.add256_u64(z3Pos, yPos, xPos)
       console.log('after')
-      for (let i = 0; i < N * 3; i++) {
-        console.log(`mem[${i}]=${u32[i]}`)
-      }
+      cmpArray(z1Pos, z2Pos)
+      cmpArray(z1Pos, z3Pos)
+      dump('z', z1Pos)
+      const C = 100000000
+      bench('extInt', C, () => { mod.add256_extInt(yPos, yPos, yPos) })
     }
   )
   return exports
