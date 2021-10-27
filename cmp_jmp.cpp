31125b475b1bc1adaafe9576a7a5e6bc0a6eb3b9
MITSUNARI Shigeo <herumi@nifty.com>
2021-10-27 18:17:20 +0900

    warm up

--- cmp_jmp.cpp
+++ cmp_jmp.cpp
@@ -1,25 +1,25 @@
-#include <xbyak/xbyak_util.h>
+#include <xbyak/xbyak.h>
 #include <cybozu/benchmark.hpp>
 
 using namespace Xbyak;
-using namespace Xbyak::util;
 
-typedef int (*funcType)(int n);
+typedef int (*funcType)();
 
 const char *g_callStr;
 
 struct Code : Xbyak::CodeGenerator {
-	bool gen(int mode)
+	bool gen(int loopN, int mode)
 	{
-		Xbyak::util::StackFrame sf(this, 1, UseRCX | UseRDX);
-		const Reg64& n = sf.p[0];
+		const Reg64& n = rcx;
+		const Reg64& addr = r8;
 
 		Label funcL, memL, tblL;
 		xor_(rax, rax);
+		mov(n, loopN);
 		switch (mode) {
 		case 0: mov(rdx, funcL); break;
 		case 1: mov(rdx, memL); break;
-		case 4: mov(rcx, tblL); break;
+		case 4: mov(addr, tblL); break;
 		default: break;
 		}
 	Label lpL = L();
@@ -28,7 +28,7 @@ struct Code : Xbyak::CodeGenerator {
 		case 1: call(ptr[rdx]); g_callStr = "call(ptr[rdx])"; break;
 		case 2: call(funcL); g_callStr = "call(funcL)"; break;
 		case 3: call(ptr[rip + memL]); g_callStr = "call(ptr[rip + memL])"; break;
-		case 4: mov(rdx, ptr[rcx]); call(ptr[rdx]); g_callStr = "mov(rdx, [rdx]); call([rdx])"; break;
+		case 4: mov(rdx, ptr[addr]); call(ptr[rdx]); g_callStr = "mov(rdx, [rdx]); call([rdx])"; break;
 		default : return false;
 		}
 		sub(n, 1);
@@ -51,9 +51,10 @@ int main()
 	const int n = 1000;
 	for (int i = 0; i < 10; i++) {
 		Code c;
-		if (!c.gen(i)) break;
+		if (!c.gen(n, i)) break;
 		funcType f = c.getCode<funcType>();
-		printf("f=%25s(%d) ", g_callStr, f(n));
-		CYBOZU_BENCH_C("call", 10000, f, n);
+		printf("f=%25s(%d) ", g_callStr, f());
+		CYBOZU_BENCH_C("", 10000, f);
+		CYBOZU_BENCH_C("call", 10000, f);
 	}
 }
