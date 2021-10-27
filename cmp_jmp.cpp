4eff454487f23319db6f747ef176f01db38b1a49
MITSUNARI Shigeo <herumi@nifty.com>
2021-10-27 18:21:06 +0900

    add unroll

--- cmp_jmp.cpp
+++ cmp_jmp.cpp
@@ -8,7 +8,7 @@ typedef int (*funcType)();
 const char *g_callStr;
 
 struct Code : Xbyak::CodeGenerator {
-	bool gen(int loopN, int mode)
+	bool gen(int loopN, int mode, int unrollN)
 	{
 		const Reg64& n = rcx;
 		const Reg64& addr = r8;
@@ -23,13 +23,15 @@ struct Code : Xbyak::CodeGenerator {
 		default: break;
 		}
 	Label lpL = L();
-		switch (mode) {
-		case 0: call(rdx); g_callStr = "call(rdx)"; break;
-		case 1: call(ptr[rdx]); g_callStr = "call(ptr[rdx])"; break;
-		case 2: call(funcL); g_callStr = "call(funcL)"; break;
-		case 3: call(ptr[rip + memL]); g_callStr = "call(ptr[rip + memL])"; break;
-		case 4: mov(rdx, ptr[addr]); call(ptr[rdx]); g_callStr = "mov(rdx, [rdx]); call([rdx])"; break;
-		default : return false;
+		for (int u = 0; u < unrollN; u++) {
+			switch (mode) {
+			case 0: call(rdx); g_callStr = "call(rdx)"; break;
+			case 1: call(ptr[rdx]); g_callStr = "call(ptr[rdx])"; break;
+			case 2: call(funcL); g_callStr = "call(funcL)"; break;
+			case 3: call(ptr[rip + memL]); g_callStr = "call(ptr[rip + memL])"; break;
+			case 4: mov(rdx, ptr[addr]); call(ptr[rdx]); g_callStr = "mov(rdx, [rdx]); call([rdx])"; break;
+			default : return false;
+			}
 		}
 		sub(n, 1);
 		jnz(lpL);
@@ -50,11 +52,13 @@ int main()
 {
 	const int n = 1000;
 	for (int i = 0; i < 10; i++) {
-		Code c;
-		if (!c.gen(n, i)) break;
-		funcType f = c.getCode<funcType>();
-		printf("f=%25s(%d) ", g_callStr, f());
-		CYBOZU_BENCH_C("", 10000, f);
-		CYBOZU_BENCH_C("call", 10000, f);
+		for (int u = 1; u < 4; u++) {
+			Code c;
+			if (!c.gen(n, i, u)) break;
+			funcType f = c.getCode<funcType>();
+			printf("f=%25s u=%d (ret=%d) ", g_callStr, u, f());
+			CYBOZU_BENCH_C("", 10000, f); // warming up
+			CYBOZU_BENCH_C("call", 10000, f);
+		}
 	}
 }
