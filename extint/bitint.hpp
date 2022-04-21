93486e812e699298f071efa1cb63cdf8322872a7
MITSUNARI Shigeo <herumi@nifty.com>
2022-04-21 17:51:07 +0900

    add cmp functions

--- extint/bitint.hpp
+++ extint/bitint.hpp
@@ -9,6 +9,15 @@ struct BitInt {
 	unsigned _ExtInt(sizeof(fp::Unit) * 8 * N) v;
 };
 
+// true if x == y
+template<size_t N>
+bool cmpEqT(const fp::Unit *px, const fp::Unit *py)
+{
+	const BitInt<N>& x = *(const BitInt<N>*)px;
+	const BitInt<N>& y = *(const BitInt<N>*)py;
+	return x.v == y.v;
+}
+
 // true if x >= y
 template<size_t N>
 bool cmpGeT(const fp::Unit *px, const fp::Unit *py)
@@ -31,14 +40,14 @@ bool cmpGtT(const fp::Unit *px, const fp::Unit *py)
 template<size_t N>
 bool cmpLeT(const fp::Unit *px, const fp::Unit *py)
 {
-	return cmpGtT<N>(py, px);
+	return !cmpGtT<N>(px, py);
 }
 
 // true if x < y
 template<size_t N>
 bool cmpLtT(const fp::Unit *px, const fp::Unit *py)
 {
-	return cmpGeT<N>(py, px);
+	return !cmpGeT<N>(px, py);
 }
 
 } } // mcl::vint
--- extint/bitint_test.cpp
+++ extint/bitint_test.cpp
@@ -6,14 +6,27 @@ typedef mcl::fp::Unit Unit;
 
 CYBOZU_TEST_AUTO(cmpT)
 {
-	const Unit x[] = { 1, 2, 4 };
-	const Unit y[] = { 1, 2, 3 };
+	const Unit x[] = { 1, 2, 4, 5 };
+	const Unit y[] = { 1, 2, 3, 8 };
+
+	CYBOZU_TEST_ASSERT(!cmpEqT<3>(x, y));
+	CYBOZU_TEST_ASSERT(cmpEqT<3>(x, x));
+	CYBOZU_TEST_ASSERT(cmpEqT<2>(x, y));
+
 	CYBOZU_TEST_ASSERT(cmpGeT<3>(x, y));
 	CYBOZU_TEST_ASSERT(cmpGeT<3>(x, x));
 	CYBOZU_TEST_ASSERT(!cmpGeT<3>(y, x));
+	CYBOZU_TEST_ASSERT(cmpGeT<2>(x, y));
+	CYBOZU_TEST_ASSERT(!cmpGeT<4>(x, y));
 
 	CYBOZU_TEST_ASSERT(cmpGtT<3>(x, y));
 	CYBOZU_TEST_ASSERT(!cmpGtT<3>(x, x));
 	CYBOZU_TEST_ASSERT(!cmpGtT<3>(y, x));
+
+	CYBOZU_TEST_ASSERT(!cmpLeT<3>(x, y));
+	CYBOZU_TEST_ASSERT(cmpLeT<3>(x, x));
+	CYBOZU_TEST_ASSERT(cmpLeT<3>(y, x));
+	CYBOZU_TEST_ASSERT(cmpLeT<2>(x, x));
+	CYBOZU_TEST_ASSERT(cmpLeT<4>(x, y));
 }
 
