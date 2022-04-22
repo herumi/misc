faa715164124f8a6630efebca71132c509844de2
MITSUNARI Shigeo <herumi@nifty.com>
2022-04-22 11:22:16 +0900

    add addT

--- extint/bitint.hpp
+++ extint/bitint.hpp
@@ -6,15 +6,36 @@ namespace mcl { namespace vint {
 
 template<size_t N>
 struct BitInt {
-	unsigned _ExtInt(sizeof(fp::Unit) * 8 * N) v;
+	static const size_t bitSize = sizeof(fp::Unit) * 8 * N;
+	typedef unsigned _ExtInt(bitSize) Type;
+	Type v;
+	fp::Unit getTopUnit() const
+	{
+		return static_cast<fp::Unit>(v >> (bitSize - sizeof(fp::Unit) * 8));
+	}
+	static const BitInt<N>& load(const void *x)
+	{
+		return *(const BitInt<N>*)x;
+	}
+	void save(void *x) const
+	{
+		*(BitInt<N>*)(x) = *this;
+	}
+	template<size_t M>
+	BitInt<M> cvt() const
+	{
+		BitInt<M> ret;
+		ret.v = static_cast<typename BitInt<M>::Type>(this->v);
+		return ret;
+	}
 };
 
 // true if x == y
 template<size_t N>
 bool cmpEqT(const fp::Unit *px, const fp::Unit *py)
 {
-	const BitInt<N>& x = *(const BitInt<N>*)px;
-	const BitInt<N>& y = *(const BitInt<N>*)py;
+	const auto& x = BitInt<N>::load(px);
+	const auto& y = BitInt<N>::load(py);
 	return x.v == y.v;
 }
 
@@ -22,8 +43,8 @@ bool cmpEqT(const fp::Unit *px, const fp::Unit *py)
 template<size_t N>
 bool cmpGeT(const fp::Unit *px, const fp::Unit *py)
 {
-	const BitInt<N>& x = *(const BitInt<N>*)px;
-	const BitInt<N>& y = *(const BitInt<N>*)py;
+	const auto& x = BitInt<N>::load(px);
+	const auto& y = BitInt<N>::load(py);
 	return x.v >= y.v;
 }
 
@@ -31,8 +52,8 @@ bool cmpGeT(const fp::Unit *px, const fp::Unit *py)
 template<size_t N>
 bool cmpGtT(const fp::Unit *px, const fp::Unit *py)
 {
-	const BitInt<N>& x = *(const BitInt<N>*)px;
-	const BitInt<N>& y = *(const BitInt<N>*)py;
+	const auto& x = BitInt<N>::load(px);
+	const auto& y = BitInt<N>::load(py);
 	return x.v > y.v;
 }
 
@@ -48,6 +69,18 @@ template<size_t N>
 bool cmpLtT(const fp::Unit *px, const fp::Unit *py)
 {
 	return !cmpGeT<N>(px, py);
+}
+
+// z = x + y and return CF(0 or 1)
+template<size_t N>
+fp::Unit addT(fp::Unit *pz, const fp::Unit *px, const fp::Unit *py)
+{
+	auto x = BitInt<N>::load(px).template cvt<N+1>();
+	auto y = BitInt<N>::load(py).template cvt<N+1>();
+	BitInt<N+1> z;
+	z.v = x.v + y.v;
+	z.template cvt<N>().save(pz);
+	return z.getTopUnit();
 }
 
 } } // mcl::vint
--- extint/bitint_test.cpp
+++ extint/bitint_test.cpp
@@ -30,3 +30,25 @@ CYBOZU_TEST_AUTO(cmpT)
 	CYBOZU_TEST_ASSERT(cmpLeT<4>(x, y));
 }
 
+CYBOZU_TEST_AUTO(addT)
+{
+	const Unit x[] = { 9, 2, 3 };
+	const Unit y[] = { Unit(-1), 5, 4 };
+	const Unit ok[] = { 8, 8, 7 };
+	Unit z[3], CF;
+	z[0] = 9;
+	z[1] = 10;
+	CF = addT<1>(z, x, y);
+	CYBOZU_TEST_EQUAL(z[0], ok[0]);
+	CYBOZU_TEST_EQUAL(CF, 1);
+	CYBOZU_TEST_EQUAL(z[1], 10); // not changed
+
+	CF = addT<1>(z, x + 1, y + 1);
+	CYBOZU_TEST_EQUAL(z[0], x[1] + y[1]);
+	CYBOZU_TEST_EQUAL(CF, 0);
+	CYBOZU_TEST_EQUAL(z[1], 10); // not changed
+
+	CF = addT<3>(z, x, y);
+	CYBOZU_TEST_EQUAL_ARRAY(z, ok, 3);
+	CYBOZU_TEST_EQUAL(CF, 0);
+}
