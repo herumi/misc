c8a5e1bd0c479db6393d75971b4f0799a942a081
MITSUNARI Shigeo <herumi@nifty.com>
2022-06-13 11:50:39 +0900

    add gen_sub

--- longint/gen_bitint_x64.py
+++ longint/gen_bitint_x64.py
@@ -20,5 +20,25 @@ def gen_add(N):
 			setc(al)
 			movzx(eax, al)
 
+def gen_sub(N):
+	proc(f'mclb_sub{N}')
+	with StackFrame(3) as sf:
+		if N == 0:
+			xor_(eax, eax)
+			return
+		else:
+			z = sf.p[0]
+			x = sf.p[1]
+			y = sf.p[2]
+			for i in range(N):
+				mov(rax, ptr(x + 8 * i))
+				if i == 0:
+					sub(rax, ptr(y + 8 * i))
+				else:
+					sbb(rax, ptr(y + 8 * i))
+				mov(ptr(z + 8 * i), rax)
+			setc(al)
+			movzx(eax, al)
+
 #setWin64ABI(True)
-gen_add(4)
+gen_sub(4)
