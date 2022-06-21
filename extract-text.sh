0d3452ead7cb2263b1b65ecfc08fd5b721c71d60
MITSUNARI Shigeo <herumi@nifty.com>
2022-06-21 20:40:44 +0900

    fix vals

--- extract-text.sh
+++ extract-text.sh
@@ -2,7 +2,9 @@
 
 SUF=aarch64-linux-gnu-
 ${SUF}as $1 -o tmp.o
-VALS=($(${SUF}objdump -h tmp.o | awk '/\.text/ { print $3 $6}'))
+VALS=($(${SUF}objdump -h tmp.o | awk '/\.text/ { printf("%s %s\n", $3, $6)}'))
 SIZE=$((0x${VALS[0]}))
 OFFSET=$((0x${VALS[1]}))
+echo size=${SIZE}
+echo offset=${OFFSET}
 dd if=tmp.o of=out.bin ibs=1 skip=${OFFSET} count=${SIZE} status=none
