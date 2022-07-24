16b62f37271c6afa29560b9785a770c992a1ce5c
MITSUNARI Shigeo <herumi@nifty.com>
2022-07-24 18:06:57 +0900

    nasm support export

--- dll/mk.bat
+++ dll/mk.bat
@@ -1,5 +1,7 @@
-python3 gen.py -win -m masm > asm.asm
-ml64 /c asm.asm
+rem python3 gen.py -win -m masm > asm.asm
+rem ml64 /c asm.asm
+python3 gen.py -win -m nasm > asm.asm
+nasm -fwin64 asm.asm
 cl /c /O2 sub.cpp
 link /nologo /DLL /OUT:bin\dll.dll asm.obj sub.obj /implib:dll.lib
 cl /O2 main.cpp dll.lib /Fe:bin\main.exe
