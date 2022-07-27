f814fb173d23c6140221a6b22c57dde7aa30603c
MITSUNARI Shigeo <herumi@nifty.com>
2022-07-27 10:22:37 +0900

    rename dll.dll to suba.dll

--- dll/mk.bat
+++ dll/mk.bat
@@ -1,8 +1,11 @@
+@echo off
+rm -f bin\*.dll bin\*.exe *.obj *.lib
 rem python3 gen.py -win -m masm > asm.asm
 rem ml64 /c asm.asm
 python3 gen.py -win -m nasm > asm.asm
 nasm -fwin64 asm.asm
 cl /c /O2 sub.cpp
-link /nologo /DLL /OUT:bin\dll.dll asm.obj sub.obj /implib:dll.lib
-cl /O2 main.cpp dll.lib /Fe:bin\main.exe
+link /nologo /DLL /OUT:bin\suba.dll asm.obj sub.obj /implib:suba.lib
+dumpbin /exports suba.lib
+cl /O2 main.cpp suba.lib /Fe:bin\main.exe
 bin\main.exe
