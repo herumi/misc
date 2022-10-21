9adb521930a6cefb539dba277a4b728d863d4a4e
MITSUNARI Shigeo <herumi@nifty.com>
2022-10-21 17:13:48 +0900

    tweak

--- dll/gen.py
+++ dll/gen.py
@@ -6,7 +6,16 @@ def gen_test():
 		with StackFrame(2) as sf:
 			x = sf.p[0]
 			y = sf.p[1]
+			L1 = Label()
+			L2 = Label()
 			lea(eax, ptr(x + y))
+			jmp(L2)
+			L(L1)
+			mov(rax, x)
+			add(rax, y)
+			ret()
+			L(L2)
+			jmp(L1)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-win', '--win', help='output win64 abi', action='store_true')
