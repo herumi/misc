311177f9e27a1f77f87d5789cb584ee5bede2fb5
MITSUNARI Shigeo <herumi@nifty.com>
2023-02-27 09:22:03 +0900

    alternative div

--- gf256/gf256_test.cpp
+++ gf256/gf256_test.cpp
@@ -22,3 +22,14 @@ CYBOZU_TEST_AUTO(mul)
 		}
 	}
 }
+
+CYBOZU_TEST_AUTO(div)
+{
+	for (uint32_t x = 0; x < 256; x++) {
+		for (uint32_t y = 1; y < 256; y++) {
+			uint8_t z1 = mul(x, gf256_inv(y));
+			uint8_t z2 = gf256_div(x, y);
+			CYBOZU_TEST_EQUAL(z1, z2);
+		}
+	}
+}
