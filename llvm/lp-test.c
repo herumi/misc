3c9529103719e46e337bfe77f5b32dc06cf1dbac
MITSUNARI Shigeo <herumi@nifty.com>
2023-03-26 16:43:48 +0900

    tweak

--- llvm/lp-test.c
+++ llvm/lp-test.c
@@ -5,7 +5,10 @@ uint32_t sum(uint32_t n);
 
 int main()
 {
+	uint32_t v1 = 0;
 	for (int i = 0; i < 10; i++) {
-		printf("%d %d\n", i, sum(i));
+		uint32_t v2 = sum(i);
+		printf("%d %u %c\n", i, v2, v1 == v2 ? 'o' : 'x');
+		v1 += i;
 	}
 }
