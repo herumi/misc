0d022f3c85ecc24d9c22efd390611ce545cdf270
MITSUNARI Shigeo <herumi@nifty.com>
2023-05-26 18:04:05 +0900

    add some op benchmark

--- fma/gen.py
+++ fma/gen.py
@@ -1,16 +1,30 @@
 from s_xbyak import *
 
-def gen_func(n):
+DATA='data'
+
+def gen_func(op, n):
   with FuncProc(f'func{n}'):
     with StackFrame(1, vNum=n, vType=T_ZMM) as sf:
       c = sf.p[0]
       lp = Label()
+      lea(rax, ptr(rip+DATA))
       for i in range(n):
         vxorps(Zmm(i), Zmm(i), Zmm(i))
 #      align(16):
       L(lp)
       for i in range(n):
-        vfmadd231ps(Zmm(i), Zmm(i), Zmm(i))
+        if op == 'fma':
+          vfmadd231ps(Zmm(i), Zmm(i), Zmm(i))
+        elif op == 'mem':
+          vfmadd231ps(Zmm(i), Zmm(i), ptr_b(rax+i*4))
+        elif op == 'add':
+          vaddps(Zmm(i), Zmm(i), Zmm(i))
+        elif op == 'mul':
+          vmulps(Zmm(i), Zmm(i), Zmm(i))
+        elif op == 'xor':
+          vxorps(Zmm(i), Zmm(i), Zmm(i))
+        else:
+          raise Exception('bad op', op)
       sub(c, 1)
       jnz(lp)
 
@@ -27,6 +41,7 @@ def gen_header(n):
 def main():
   parser = getDefaultParser()
   parser.add_argument('-header', '--header', help='generate a header', action='store_true')
+  parser.add_argument('-op', '--op', help='operand', type=str, default='fma')
   param = parser.parse_args()
 
   N=10
@@ -36,10 +51,14 @@ def main():
     return
 
   init(param)
+  segment('data')
+  makeLabel(DATA)
+  for i in range(16):
+    dd_(i)
   segment('text')
 
   for i in range(1, N+1):
-    gen_func(i)
+    gen_func(param.op, i)
 
   term()
 
