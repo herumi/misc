8868e20387a2edeb84064a22f168704e026c0dd9
MITSUNARI Shigeo <herumi@nifty.com>
2023-05-28 17:18:15 +0900

    add opt

--- fma/fma_test.cpp
+++ fma/fma_test.cpp
@@ -1,4 +1,5 @@
 #include <cybozu/benchmark.hpp>
+#include <cybozu/option.hpp>
 
 #include "func.h"
 
@@ -8,11 +9,30 @@ template<int n>
 void bench()
 {
 	int C = 100;
-	CYBOZU_BENCH_C("", 100, funcN<n>, N);
+	CYBOZU_BENCH_C("", C, funcN<n>, N);
     printf("n=%d %f\n", n, cybozu::bench::g_clk.getClock()/double(N)/C);
 }
-int main()
+
+int main(int argc, char *argv[])
 {
+	cybozu::Option opt;
+	int n;
+	opt.appendOpt(&n, 0, "n", "unroll n");
+	if (!opt.parse(argc, argv)) {
+		opt.usage();
+		return 0;
+	}
+	if (n > 0) {
+		switch (n) {
+		case 4: bench<4>(); break;
+		case 7: bench<7>(); break;
+		case 8: bench<8>(); break;
+		default:
+			printf("not support n=%d\n", n);
+			break;
+		}
+		return 0;
+	}
 	bench<1>();
 	bench<2>();
 	bench<3>();
