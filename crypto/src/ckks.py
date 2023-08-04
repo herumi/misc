696b36257329fb354bf48b6e8df261e68dfc2c23
MITSUNARI Shigeo <herumi@nifty.com>
2023-08-04 10:28:54 +0900

    _

--- crypto/src/ckks.py
+++ crypto/src/ckks.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 from numpy.polynomial import Polynomial as poly
 from math import *
@@ -11,42 +12,42 @@ def getRand(n):
 #  return secrets.randbelow(n)
 
 class Param:
-  def __init__(self, Delta = 1000):
+  def __init__(self, delta = 1000):
     self.a = 0
     self.M = 0
     self.N = 0
     self.halfN = 0
     self.xi = 0
-    self.A = []
-    self.invA = None
+    self.S = []
+    self.invS = None
     self.cycloPoly = None
-    self.Delta = Delta
+    self.delta = delta
     self.l = 2
     self.L = 3
-    self.p = 1000
+    self.p = 10000
     self.q0 = 10000
     self.P = 10000
     self.qL = (self.p ** self.L) * self.q0
 
 # g_M : power of two
-# g_Delta : scale
+# g_delta : scale
 # L : max num of mul
-def init(M: int, Delta = 10000):
+def init(M: int, delta = 1000):
   global g_
-  g_ = Param(Delta)
+  g_ = Param(delta)
   g_.M = M
   g_.N = M//2
   g_.halfN = g_.N//2
-  g_.xi = np.exp(2 * np.pi * 1j / g_.M)
+  g_.xi = np.exp(2 * pi * 1j / g_.M)
 
-  # g_.A = (a_ij) = (((g_.xi^(2*i + 1))^j)
+  # g_.S = (a_ij) = (((g_.xi^(2*i + 1))^j)
   for i in range(g_.N):
     root = g_.xi ** (2 * i + 1)
     row = []
     for j in range(g_.N):
       row.append(root ** j)
-    g_.A.append(row)
-  g_.invA = np.linalg.inv(g_.A)
+    g_.S.append(row)
+  g_.invS = np.linalg.inv(g_.S)
 
   # M-th cyclotomic poly : phi_M(X) = X^N + 1
   g_.cycloPoly = poly([1] + [0] * (g_.N-1) + [1])
@@ -61,30 +62,30 @@ def putMatrix(a):
   for v in a:
     putVector(v)
 
-def get_ql(l:int) -> int:
+def get_ql(l:int):
   return g_.p ** l * g_.q0
 
-# Sigma(p) = (p(g_.xi^(2*i+1)))
-def Sigma(p: poly) -> np.array:
+# sigma(p) = (p(g_.xi^(2*i+1)))
+def sigma(p: poly):
   f = []
   for i in range(g_.N):
-    f.append(p(g_.A[i][1]))
+    f.append(p(g_.S[i][1]))
   return np.array(f)
 
-def invSigma(b: np.array) -> poly:
-  return poly(np.dot(g_.invA, b))
+def invSigma(b: np.array):
+  return poly(np.dot(g_.invS, b))
 
-# Projection from C^N -> C^(N/2)
-def Pi(z: np.array) -> np.array:
+# Projection from C^N → C^(N/2)
+def proj(z: np.array):
   return z[0:g_.halfN]
 
 # convert a in C^(N/2) to [a:reverse(conj(a))] in C^N
-def invPi(z: np.array) -> np.array:
+def invProj(z: np.array):
   assert z.shape[0] == g_.halfN
   w = z.conjugate()[::-1]
   return np.hstack([z, w])
 
-def getRealPoly(p: poly) -> poly:
+def getRealPoly(p: poly):
   def f(x):
     if x.imag != 0:
       raise('not zero')
@@ -94,47 +95,44 @@ def getRealPoly(p: poly) -> poly:
   return poly(list(map(f, p)))
 
 # Round up coefficients of a polynomial
-def roundCoeff(p: poly) -> poly:
+def roundCoeff(p: poly):
   if type(p) == poly:
     p = p.convert().coef
   p = poly(list(map(np.round, p)))
   return p
 
-def signMod(x, n):
+def mod(x, n):
   t = x % n
-  if x >= 0:
-    return t
-  return t - n
+  if t >= n//2:
+    t -= n
+  return t
 
 # modulo coefficients of a polynomial
-def modCoeff(p: poly, m) -> poly:
+def modCoeff(p: poly, m):
   def f(x):
-    a = signMod(x.real, m)
-    b = signMod(x.imag, m)
+    a = mod(x.real, m)
+    b = mod(x.imag, m)
     if b == 0:
       return a
     return a + b * 1j
   return poly(list(map(f, p.convert().coef)))
 
-def Encode(z: np.array) -> poly:
+def Ecd(z: np.array):
   assert z.shape[0] == g_.halfN
-  # scale
-  h = invSigma(invPi(z) * g_.Delta)
-  # round
-  p = roundCoeff(h)
-  # get real part
+  p = invSigma(invProj(z)) * g_.delta
+  p = roundCoeff(p)
   p = getRealPoly(p)
   return p
 
-def Decode(m: poly) -> np.array:
-  return Pi(Sigma(m) / g_.Delta)
+def Dcd(m: poly):
+  return proj(sigma(m) / g_.delta)
 
 def randZERO():
   return poly([0] * g_.N)
 
 # QQQ : consider Hamming weight h
 # reutrn {-1, 0, 1}^N
-def randHWT() -> poly:
+def randHWT():
   a = []
   for i in range(g_.N):
     v = getRand(3) - 1
@@ -142,7 +140,7 @@ def randHWT() -> poly:
   return poly(a)
 
 # rho = 0.5 (Pr(+1)=Pr(-1)=1/4, Pr(0)=1/2)
-def randZO() -> poly:
+def randZO():
   a = []
   for i in range(g_.N):
     v = getRand(4)
@@ -163,14 +161,14 @@ def randPoly(w):
   return poly(a)
 
 # QQQ : consider variance
-def randDG() -> poly:
+def randDG():
   return randPoly(3)
 
 # R_{qL}
-def randRQL() -> poly:
-  return randPoly(100)
+def randRQL():
+  return randPoly(100) # QQQ
 
-def modPoly(p: poly) -> poly:
+def modPoly(p: poly):
   return p % g_.cycloPoly
 
 def randPlainText(rand=True):
@@ -217,12 +215,12 @@ def Dec(sk, c):
   t = modCoeff(t, ql)
   return t
 
-def Add(c1, c2):
+def add(c1, c2):
   b1, a1 = c1
   b2, a2 = c2
   return (b1 + b2, a1 + a2)
 
-def Mul(c1, c2, ek=None):
+def mul(c1, c2, ek=None):
   b1, a1 = c1
   b2, a2 = c2
   ql = get_ql(g_.l)
@@ -239,7 +237,17 @@ def Mul(c1, c2, ek=None):
   t1 = modCoeff(t1, ql)
   return (t0, t1)
 
-# X -> X^k
+def rescale(c, l, lp):
+  """
+  convert c in R_{q^l} to R_{q^lp}
+  """
+  t0, t1= c
+  d = g_.p ** (l - lp)
+  t0 = roundCoeff(t0 / d)
+  t1 = roundCoeff(t1 / d)
+  return (t0, t1)
+
+# X → X^k
 def Permutate(f, k):
   co = f.convert().coef
   g = [co[0]]
@@ -260,15 +268,15 @@ def main():
   M = 8
   init(M)
 
-  print('Encode / Decode')
+  print('Ecd / Dcd')
   if M == 8:
     z = np.array([3+4j, 2-1j])
   else:
     z = randPlainText(False)
   print(f'{z=}')
-  m = Encode(z)
+  m = Ecd(z)
   print(f'enc={m}')
-  print(f'dec={Decode(m)}')
+  print(f'dec={Dcd(m)}')
 
   print('\n\nHWT')
   for i in range(5):
@@ -280,15 +288,15 @@ def main():
   PUT('pk', pk)
   PUT('ek', ek)
 
-  PUT('z  ', Decode(m))
+  PUT('z  ', Dcd(m))
   PUT('msg', m)
   c = Enc(pk, m)
   PUT('enc', c)
   d = Dec(sk, c)
   PUT('dec', d)
-  PUT('dcd', Decode(d))
+  PUT('dcd', Dcd(d))
 
-  print('\n\nAdd / Mul')
+  print('\n\nAdd / mul')
   z1 = randPlainText()
   z2 = randPlainText()
 #  if M == 8:
@@ -296,12 +304,12 @@ def main():
 #    z2 = np.array([-2 + 4j, -5 + 1j])
   PUT('z1', z1)
   PUT('z2', z2)
-  m1 = Encode(z1)
-  m2 = Encode(z2)
+  m1 = Ecd(z1)
+  m2 = Ecd(z2)
   PUT('m1', m1)
   PUT('m2', m2)
-  org1 = Decode(m1)
-  org2 = Decode(m2)
+  org1 = Dcd(m1)
+  org2 = Dcd(m2)
   PUT('org1', org1)
   PUT('org2', org2)
   c1 = Enc(pk, m1)
@@ -310,18 +318,23 @@ def main():
   PUT('c2', c2)
   PUT('dec c1', Dec(sk, c1))
   PUT('dec c2', Dec(sk, c2))
-  c = Add(c1, c2)
+  c = add(c1, c2)
   PUT('add', c)
   d = Dec(sk, c)
   PUT('dec', d)
-  PUT('dcd', Decode(d))
+  PUT('dcd', Dcd(d))
   PUT('org', org1 + org2)
-  c = Mul(c1, c2, ek)
+  c = mul(c1, c2, ek)
   PUT('mul', c)
   d = Dec(sk, c)
   PUT('dec', d)
-  PUT('dcd', Decode(d) / g_.Delta)
+  PUT('dcd', Dcd(d) / g_.delta)
   PUT('org', org1 * org2)
+  c3 = mul(c, c2, ek)
+  PUT('dcd c3', Dcd(Dec(sk, c3))/g_.delta**2)
+  PUT('org', org1 * org2 * org2)
+#  cc = rescale(c3, 2, 1)
+#  PUT('dcd cc', Dcd(Dec(sk, cc))/g_.delta)
 
 
 if __name__ == '__main__':
