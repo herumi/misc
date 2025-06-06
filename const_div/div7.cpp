582d477bc74fd5dbf5b8411aa2ad9ff5c6e776da
MITSUNARI Shigeo <herumi@nifty.com>
2025-06-06 09:21:25 +0900

    use xorshift

--- const_div/div7.cpp
+++ const_div/div7.cpp
@@ -1,7 +1,23 @@
-
 #include <stdint.h>
 
+#ifdef _MSC_VER
+#include <intrin.h>
+#elif defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
 typedef __attribute__((mode(TI))) unsigned int uint128_t;
+#define MCL_DEFINED_UINT128_T
+#endif
+
+inline uint64_t mulUnit1(uint64_t *pH, uint64_t x, uint64_t y)
+{
+#ifdef MCL_DEFINED_UINT128_T
+	uint128_t t = uint128_t(x) * y;
+	*pH = uint64_t(t >> 64);
+	return uint64_t(t);
+#else
+	return _umul128(x, y, pH);
+#endif
+}
+
 const uint64_t u_ = 0x124924925;
 const uint32_t a_=35;
 
@@ -14,8 +30,16 @@ uint32_t div7org(uint32_t x)
 
 uint32_t div7a(uint32_t x)
 {
-    uint128_t v = (x * uint128_t(u_)) >> a_;
-    return uint32_t(v);
+#ifdef MCL_DEFINED_UINT128_T
+	uint128_t v = (x * uint128_t(u_)) >> a_;
+	return uint32_t(v);
+#else
+	uint64_t H;
+	uint64_t L = mulUnit1(&H, x, u_);
+	L >>= a_;
+	H <<= (64 - a_);
+	return uint32_t(H | L);
+#endif
 }
 
 uint32_t div7b(uint32_t x)
--- const_div/div7_test.cpp
+++ const_div/div7_test.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
+#ifndef _MSC_VER
+#define CYBOZU_BENCH_USE_GETTIMEOFDAY
+#endif
 #include <cybozu/benchmark.hpp>
+#include <cybozu/xorshift.hpp>
 
 extern "C" {
 
@@ -12,9 +17,10 @@ uint32_t div7b(uint32_t x);
 
 uint64_t sumAll(uint32_t (*f)(uint32_t), uint32_t n)
 {
+	cybozu::XorShift rg;
 	uint64_t sum = 0;
-	for (uint64_t x_ = 0; x_ <= n; x_++) {
-		uint32_t x = uint32_t(x_);
+	for (uint64_t x_ = 1; x_ < n; x_++) {
+		uint32_t x = rg.get32();
 		sum += f(x);
 	}
 	return sum;
@@ -22,15 +28,18 @@ uint64_t sumAll(uint32_t (*f)(uint32_t), uint32_t n)
 
 int main()
 {
-	const uint32_t n = 100000000;
-	const int C = 10;
+	const uint32_t n = 10000000;
+	const int C = 100;
 
 	uint64_t sum0 = 0, sum1 = 0, sum2 = 0;
 	CYBOZU_BENCH_C("org", C, sum0 += sumAll, div7org, n);
 	CYBOZU_BENCH_C("a  ", C, sum1 += sumAll, div7a, n);
 	CYBOZU_BENCH_C("b  ", C, sum2 += sumAll, div7b, n);
 	CYBOZU_BENCH_C("org", C, sum0 += sumAll, div7org, n);
-	printf("sum=%lx %lx %lx\n", sum0/2, sum1, sum2);
+	CYBOZU_BENCH_C("a  ", C, sum1 += sumAll, div7a, n);
+	CYBOZU_BENCH_C("b  ", C, sum2 += sumAll, div7b, n);
+	printf("sum=%" PRIx64 " %" PRIx64 " %" PRIx64 "\n", sum0, sum1, sum2);
+#if 0
 	#pragma omp parallel for
 	for (uint64_t x_ = 0; x_ <= 0xffffffff; x_++) {
 		uint32_t x = uint32_t(x_);
@@ -42,4 +51,5 @@ int main()
 		}
 	}
 	puts("ok");
+#endif
 }
