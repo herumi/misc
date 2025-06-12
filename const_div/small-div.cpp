db11c03859ed57e8f20b3817c31fb8f1dfc65331
MITSUNARI Shigeo <herumi@nifty.com>
2025-06-12 15:40:51 +0900

    proof of equivalence between my formula and optimal bounds paper

--- const_div/small-div.cpp
+++ const_div/small-div.cpp
@@ -28,63 +28,72 @@ inline uint64_t mulUnit1(uint64_t *pH, uint64_t x, uint64_t y)
 #endif
 }
 
-uint32_t floor_ilog2(uint32_t p)
+uint32_t floor_ilog2(uint32_t x)
 {
 	uint32_t a = 0;
-	while ((one << a) <= p) a++;
+	while ((one << a) <= x) a++;
 	return a - 1;
 }
 
-uint32_t ceil_ilog2(uint32_t p)
+uint32_t ceil_ilog2(uint32_t x)
 {
 	uint32_t a = 0;
-	while ((one << a) < p) a++;
+	while ((one << a) < x) a++;
 	return a;
 }
 
 /*
-M = 2**32 - 1
-p in [1, M]
-2^a = p u - e, 0 <= e <= p-1
-x in [0, M]. (q, r) := divmod(x, p). x = p q + r.
-Then x u = p q u + r0 u = (2^a + e) q + r u = 2^a q + (e q + r u)
-y := q e + r u.
-If 0 <= y < 2^a, then q = (x u) >> a.
-To satisfy the condition, compute max(y).
-q0 := M//p. r0 := M%p.
-1-1) q = q0 and r = r0 then y = q0 e + r0 u
-1-2) q = q0-1 and r0 = p-1 then y = (q0-1)e + (p-1)u
-Then max(q0 e + r0 u, (q0-1)e + (p-1)u) < 2^a.
+M: integer >= 1.
+d in [1, M]
+take a. A := 2^a >= d. c := (A + d - 1) // d.
+e = d c - A. 0 <= e <= d-1 < A.
+(q0, r0) = divmod(M, d). M = q0 d + r0.
+M0 := M - ((M+1)%d). Then M0%d = d-1.
+Theorem
+if e M0 < A, then (x c)//A = x//d. for x in [0, M].
+Proof
+(q, r) := divmod(x, d). x = d q + r.
+Then x c = d q c + r c = (A + e) q + r c = A q + (q e + r c).
+y := q e + r c.
+If 0 <= y < A, then q = (x c) >> a.
+So we prove that y < A if and only if e M0 < A.
+y d = q e d + r c d = e q d + r (e + A) = e(d q + r) + r A = e x + r A.
+Consider max(y d).
+if r0 = d-1, then max(y d) (at x=M) = e M + (d-1) A = e M0 + (d-1) A.
+if r0 < d-1, then max(y d) (at x=M0) = e M0 + (d-1) A because e < A.
+Then max (y d) = e M0 + (d-1)A.
+max(y) < A iff max(y d) < d A iff e M0 + (d-1) A < d A iff e M0 < A.
+e M0 < A iff c/A < (1 + 1/M0)/d. This condition is the assumption of Thereom 1
+in Integer division by constants: optimal bounds.
 */
 struct MyAlgo {
-	uint32_t p_;
+	uint32_t d_;
 	uint32_t a_;
 	uint64_t A_;
-	uint64_t u_;
-	MyAlgo() : p_(0), a_(0), A_(0), u_(0) {}
+	uint64_t c_;
+	MyAlgo() : d_(0), a_(0), A_(0), c_(0) {}
 	void put() const
 	{
-		printf("My p=%u(0x%08x) a=%u u=0x%" PRIx64 "\n", p_, p_, a_, u_);
+		printf("My d=%u(0x%08x) a=%u u=0x%" PRIx64 "\n", d_, d_, a_, c_);
 	}
-	bool init(uint32_t p)
+	bool init(uint32_t d)
 	{
-		assert(p <= M);
-		uint32_t q0 = M / p;
-		uint32_t r0 = M % p;
-		// u > 0 => A >= p => a >= ilog2(p)
-		for (uint32_t a = floor_ilog2(p); a < 64; a++) {
+		assert(d <= M);
+		uint32_t M0 = M - ((M+1)%d);
+		// u > 0 => A >= d => a >= ilog2(d)
+		for (uint32_t a = floor_ilog2(d); a < 64; a++) {
 			uint64_t A = one << a;
-			uint64_t u = (A + p - 1) / p;
-			assert(u < (one << 33));
-			if (u >= (one << 33)) continue; // same result if this line is comment out.
-			uint64_t e = p * u - A;
-//			if ((q0-1) * e + (p-1) * u < A && q0 * e + r0 * u < A) { // better
-//			if (q0 * e < u && (q0+1) * e < (p-r0) * u) { // equivalent
-			if (e * (M-((M+1)%p)) < A) { // Integer division by constants: optimal bounds. Theorem 1
-				p_ = p;
+			uint64_t c = (A + d - 1) / d;
+			assert(c < (one << 33));
+			if (c >= (one << 33)) continue; // same result if this line is comment out.
+			uint64_t e = d * c - A;
+			assert(e < A);
+			if (e * M0 < A) {
+				assert(e < c);
+				d_ = d;
 				a_ = a;
 				A_ = A;
-				u_ = u;
+				c_ = c;
 				return true;
 			}
 		}
@@ -92,27 +101,27 @@ struct MyAlgo {
 	}
 	uint32_t divp(uint32_t x) const
 	{
-		if (u_ > 0xffffffff) {
+		if (c_ > 0xffffffff) {
 #ifdef MCL_DEFINED_UINT128_T
-			uint128_t v = (x * uint128_t(u_)) >> a_;
+			uint128_t v = (x * uint128_t(c_)) >> a_;
 			return uint32_t(v);
 #else
 #if 1
-			uint64_t v = x * (u_ & 0xffffffff);
+			uint64_t v = x * (c_ & 0xffffffff);
 			v >>= 32;
 			v += x;
 			v >>= a_-32;
 			return uint32_t(v);
 #else
 			uint64_t H;
-			uint64_t L = mulUnit1(&H, x, u);
+			uint64_t L = mulUnit1(&H, x, c);
 			L >>= a;
 			H <<= (64 - a);
 			return uint32_t(H | L);
 #endif
 #endif
 		} else {
-			uint32_t v = uint32_t((x * u_) >> a_);
+			uint32_t v = uint32_t((x * c_) >> a_);
 			return v;
 		}
 	}
@@ -121,29 +130,29 @@ struct MyAlgo {
 // Division by Invariant Integers using Multiplication. Granlund, Montgomery
 // https://dl.acm.org/doi/10.1145/178243.178249
 struct GM {
-	uint32_t p_;
+	uint32_t d_;
 	uint32_t l_;
 	uint64_t mH_;
 	uint32_t sh_;
 	uint32_t a_; // same as MyAlgo
 	void put() const
 	{
-		printf("GM p=%u(0x%08x) l=%u mH=0x%" PRIx64 " sh=0x%x a=%u\n", p_, p_, l_, mH_, sh_, a_);
+		printf("GM d=%u(0x%08x) l=%u mH=0x%" PRIx64 " sh=0x%x a=%u\n", d_, d_, l_, mH_, sh_, a_);
 	}
-	bool init(uint32_t p)
+	bool init(uint32_t d)
 	{
-		uint32_t l = ceil_ilog2(p);
+		uint32_t l = ceil_ilog2(d);
 		uint32_t sh = l;
 		uint64_t a = one << (N+l);
-		uint64_t L = a / p;
-//		uint64_t mH = (a + (one << l)) / p; // original Figure 6.2 of GM
-		uint64_t mH = (a + (a / (M - (M % p)))) / p; // better parameter
+		uint64_t L = a / d;
+//		uint64_t mH = (a + (one << l)) / d; // original Figure 6.2 of GM
+		uint64_t mH = (a + (a / (M - (M % d)))) / d; // better parameter
 		while ((L>>1) < (mH>>1) && sh > 0) {
 			L >>= 1;
 			mH >>= 1;
 			sh--;
 		}
-		p_ = p;
+		d_ = d;
 		l_ = l;
 		mH_ = mH;
 		sh_ = sh;
@@ -167,21 +176,21 @@ template<class Algo>
 void check(const Algo& algo, uint32_t x)
 {
 	uint32_t q = algo.divp(x);
-	uint32_t r = x - q * algo.p_;
-	if (r >= algo.p_) {
+	uint32_t r = x - q * algo.d_;
+	if (r >= algo.d_) {
 		algo.put();
 		printf("ERR x=%u\n", x);
-		printf("ok=%u err=%u\n", q / algo.p_, q);
+		printf("ok=%u err=%u\n", q / algo.d_, q);
 		exit(1);
 	}
 }
 
 template<class Algo>
-void checkAll(uint32_t p)
+void checkAll(uint32_t d)
 {
 	Algo algo;
-	if (!algo.init(p)) {
-		printf("ERR algo.init(%u)\n", p);
+	if (!algo.init(d)) {
+		printf("ERR algo.init(%u)\n", d);
 		return;
 	}
 	algo.put();
@@ -196,8 +205,8 @@ template<class Algo>
 void checkSomeP(const uint32_t *tbl, size_t tblN)
 {
 	for (size_t i = 0; i < tblN; i++) {
-		uint32_t p = tbl[i];
-		checkAll<Algo>(p);
+		uint32_t d = tbl[i];
+		checkAll<Algo>(d);
 	}
 }
 
@@ -220,18 +229,18 @@ int main()
 	{
 		puts("find diff");
 		#pragma omp parallel for
-		for (int64_t p_ = 3; p_ <= 0xfffffffe; p_ += 2) {
-			uint32_t p = uint32_t(p_);
+		for (int64_t d_ = 3; d_ <= 0xfffffffe; d_ += 2) {
+			uint32_t d = uint32_t(d_);
 			MyAlgo my;
 			GM gm;
-			bool b1 = my.init(p);
-			bool b2 = gm.init(p);
+			bool b1 = my.init(d);
+			bool b2 = gm.init(d);
 			if (b1 && b2) {
 				if (my.a_ != gm.a_) {
-//					printf("p=%08x my=%u gm=%u\n", p, my.a_, gm.a_);
+//					printf("d=%08x my=%u gm=%u\n", d, my.a_, gm.a_);
 				}
-				if (my.u_ <= M && gm.mH_ > M) {
-					printf("p=0x%08x my.u=%" PRIx64 " a=%u gm.u=%" PRIx64 " a=%u\n", p, my.u_, my.a_, gm.mH_, gm.a_);
+				if (my.c_ <= M && gm.mH_ > M) {
+					printf("d=0x%08x my.c=%" PRIx64 " a=%u gm.c=%" PRIx64 " a=%u\n", d, my.c_, my.a_, gm.mH_, gm.a_);
 				}
 			}
 		}
@@ -240,13 +249,14 @@ int main()
 #endif
 
 
-#if 0
+#if 1
 	puts("all check");
 	#pragma omp parallel for
-	for (uint32_t p = 0x7fffffff; p <= 0xfffffffe; p += 2) {
+	for (uint32_t d = 1; d <= 0xb0000000; d++) {
 		MyAlgo algo;
-		if (algo.init(p)) {
-			checkAll(algo);
+		if (!algo.init(d)) {
+			printf("ERR d=%u(0x%08x)\n", d, d);
+//			checkAll(algo);
 		}
 	}
 	puts("ok");
