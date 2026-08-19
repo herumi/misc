45e051e5aed04f8cc37e8ab3515924b4e7c2a008
MITSUNARI Shigeo <herumi@nifty.com>
2026-08-19 16:30:29 +0900

    simplify

--- cyc-mul-addi.cpp
+++ cyc-mul-addi.cpp
@@ -7,16 +7,18 @@ typedef void (*Func)();
 
 static const uint64_t N = 1000000000;
 static const int UNROLL = 8;
+static double g_rate;
 
 struct Code : Xbyak::CodeGenerator {
 	Code()
 		: f0(gen(0))
 		, f1(gen(1))
 		, f2(gen(2))
+		, f3(gen(3))
 	{
 
 	}
-	Func f0, f1, f2;
+	Func f0, f1, f2, f3;
 	Func gen(int mode)
 	{
 		Func func = getCurr<Func>();
@@ -34,6 +36,9 @@ struct Code : Xbyak::CodeGenerator {
 		case 2:
 			for (int i = 0; i < UNROLL * 2; i++) add(rax, i + 1);
 			break;
+		case 3:
+			mulx(rax, rax, rax);
+			break;
 		}
 		dec(ecx);
 		jnz(lpL);
@@ -42,7 +47,7 @@ struct Code : Xbyak::CodeGenerator {
 	}
 } s_c;
 
-double measure(Func f)
+double measureBase(Func f)
 {
 	Clock clk;
 	clk.begin();
@@ -55,18 +60,26 @@ double getCycleRate()
 {
 	Func f = s_c.f0;
 	const int n = 5;
-	measure(f);
+	fprintf(stderr, "warming up\n");
+	measureBase(f);
+	fprintf(stderr, "calc rate\n");
 	double sum = 0;
 	for (int i = 0; i < n; i++) {
-		sum += measure(f) / UNROLL;
+		sum += measureBase(f) / UNROLL;
 	}
 	return sum / n;
 }
 
+void measure(const char *msg, Func f)
+{
+	printf("%s %.2f\n", msg, measureBase(f) / g_rate);
+}
+
 int main()
 {
-	const double rate = getCycleRate();
-	printf("rate=%f\n", rate);
-	printf("mul %.2f cyc\n", measure(s_c.f1) / rate);
-	printf("addi %.2f cyc\n", measure(s_c.f2) / rate);
+	g_rate = getCycleRate();
+	fprintf(stderr, "rate=%f\n", g_rate);
+	measure("mul", s_c.f1);
+	measure("addi", s_c.f2);
+	measure("mulx", s_c.f3);
 }
